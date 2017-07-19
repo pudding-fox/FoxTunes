32c6ed03ca47298b40c3c2f8c15ad4c897aa7bb2
aidang <aidan.gilbert@thisisnumero.com>
2017-07-19 17:08:36 +0100

    Pause playback while adjusting stream position.

---
+++ FoxTunes.UI.Windows/ViewModel/StreamPosition.cs
@@ -0,0 +1,46 @@
+﻿using FoxTunes.Interfaces;
+using System.Windows;
+using System.Windows.Input;
+
+namespace FoxTunes.ViewModel
+{
+    public class StreamPosition : ViewModelBase
+    {
+        public ICommand PauseCommand
+        {
+            get
+            {
+                return new Command<IPlaybackManager>(playback =>
+                    {
+                        if (playback.CurrentStream.IsPlaying)
+                        {
+                            playback.CurrentStream.Pause();
+                        }
+                    },
+                    playback => playback != null && playback.CurrentStream != null
+                );
+            }
+        }
+
+        public ICommand ResumeCommand
+        {
+            get
+            {
+                return new Command<IPlaybackManager>(playback =>
+                    {
+                        if (playback.CurrentStream.IsPaused)
+                        {
+                            playback.CurrentStream.Resume();
+                        }
+                    },
+                    playback => playback != null && playback.CurrentStream != null
+                );
+            }
+        }
+
+        protected override Freezable CreateInstanceCore()
+        {
+            return new StreamPosition();
+        }
+    }
+}
