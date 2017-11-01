2fd6416e80dcdba02e68f8db25c2ffbe8a4b136b
aidan <aidan.gilbert@live.co.uk>
2017-11-01 23:29:27 +0000

    Executes library hierarchy scripts in parallel.
     Actually doesn't seem to make a difference to
     the build time.

--- FoxTunes.Core/Utilities/MetaDataPopulator.cs
+++ FoxTunes.Core/Utilities/MetaDataPopulator.cs
@@ -8,13 +8,13 @@ using System.Threading;
 
 namespace FoxTunes
 {
-    public class MetaDataPopulator : BaseComponent, IReportsProgress, IDisposable
+    public class MetaDataPopulator : PopulatorBase
     {
         public readonly object SyncRoot = new object();
 
         private MetaDataPopulator()
         {
-            this.Commands = new ThreadLocal<MetaDataPopulatorCommands>(true);
+            this.Command = new ThreadLocal<MetaDataPopulatorCommand>(true);
         }
 
         public MetaDataPopulator(IDatabaseContext databaseContext, IDbTransaction transaction, string prefix) : this()
@@ -32,121 +32,7 @@ namespace FoxTunes
 
         public IMetaDataSourceFactory MetaDataSourceFactory { get; private set; }
 
-        private ThreadLocal<MetaDataPopulatorCommands> Commands { get; set; }
-
-        private string _Name { get; set; }
-
-        public string Name
-        {
-            get
-            {
-                return this._Name;
-            }
-            protected set
-            {
-                this._Name = value;
-                this.OnNameChanged();
-            }
-        }
-
-        protected virtual void OnNameChanged()
-        {
-            if (this.NameChanged != null)
-            {
-                this.NameChanged(this, EventArgs.Empty);
-            }
-            this.OnPropertyChanged("Name");
-        }
-
-        public event EventHandler NameChanged = delegate { };
-
-        private string _Description { get; set; }
-
-        public string Description
-        {
-            get
-            {
-                return this._Description;
-            }
-            protected set
-            {
-                this._Description = value;
-                this.OnDescriptionChanged();
-            }
-        }
-
-        protected virtual void OnDescriptionChanged()
-        {
-            if (this.DescriptionChanged != null)
-            {
-                this.DescriptionChanged(this, EventArgs.Empty);
-            }
-            this.OnPropertyChanged("Description");
-        }
-
-        public event EventHandler DescriptionChanged = delegate { };
-
-        private int _Position { get; set; }
-
-        public int Position
-        {
-            get
-            {
-                return this._Position;
-            }
-            protected set
-            {
-                this._Position = value;
-                this.OnPositionChanged();
-            }
-        }
-
-        protected virtual void OnPositionChanged()
-        {
-            if (this.PositionChanged != null)
-            {
-                this.PositionChanged(this, EventArgs.Empty);
-            }
-            this.OnPropertyChanged("Position");
-        }
-
-        public event EventHandler PositionChanged = delegate { };
-
-        private int _Count { get; set; }
-
-        public int Count
-        {
-            get
-            {
-                return this._Count;
-            }
-            protected set
-            {
-                this._Count = value;
-                this.OnCountChanged();
-            }
-        }
-
-        protected virtual void OnCountChanged()
-        {
-            if (this.CountChanged != null)
-            {
-                this.CountChanged(this, EventArgs.Empty);
-            }
-            this.OnPropertyChanged("Count");
-        }
-
-        public event EventHandler CountChanged = delegate { };
-
-        public bool IsIndeterminate
-        {
-            get
-            {
-                return false;
-            }
-        }
-
-        public event EventHandler IsIndeterminateChanged = delegate { };
+        private ThreadLocal<MetaDataPopulatorCommand> Command { get; set; }
 
         public override void InitializeComponent(ICore core)
         {
@@ -169,19 +55,19 @@ namespace FoxTunes
             {
                 Logger.Write(this, LogLevel.Debug, "Populating meta data for file data: {0} => {1}", fileData.Id, fileData.FileName);
 
-                var commands = this.GetOrAddCommands();
+                var command = this.GetOrAddCommands();
                 var metaDataSource = this.MetaDataSourceFactory.Create(fileData.FileName);
 
-                commands.MetaDataParameters["itemId"] = fileData.Id;
+                command.Parameters["itemId"] = fileData.Id;
 
                 foreach (var metaDataItem in metaDataSource.MetaDatas)
                 {
-                    commands.MetaDataParameters["name"] = metaDataItem.Name;
-                    commands.MetaDataParameters["type"] = metaDataItem.Type;
-                    commands.MetaDataParameters["numericValue"] = metaDataItem.NumericValue;
-                    commands.MetaDataParameters["textValue"] = metaDataItem.TextValue;
-                    commands.MetaDataParameters["fileValue"] = metaDataItem.FileValue;
-                    commands.MetaDataCommand.ExecuteNonQuery();
+                    command.Parameters["name"] = metaDataItem.Name;
+                    command.Parameters["type"] = metaDataItem.Type;
+                    command.Parameters["numericValue"] = metaDataItem.NumericValue;
+                    command.Parameters["textValue"] = metaDataItem.TextValue;
+                    command.Parameters["fileValue"] = metaDataItem.FileValue;
+                    command.Command.ExecuteNonQuery();
                 }
 
                 if (position % interval == 0)
@@ -197,63 +83,46 @@ namespace FoxTunes
             });
         }
 
-        private MetaDataPopulatorCommands GetOrAddCommands()
+        private MetaDataPopulatorCommand GetOrAddCommands()
         {
-            if (this.Commands.IsValueCreated)
+            if (this.Command.IsValueCreated)
             {
-                return this.Commands.Value;
+                return this.Command.Value;
             }
-            return this.Commands.Value = new MetaDataPopulatorCommands(this.DatabaseContext, this.Transaction, this.Prefix);
+            return this.Command.Value = new MetaDataPopulatorCommand(this.DatabaseContext, this.Transaction, this.Prefix);
         }
-
-        public bool IsDisposed { get; private set; }
-
-        public void Dispose()
+        
+        protected override void OnDisposing()
         {
-            this.Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (this.IsDisposed || !disposing)
+            foreach (var command in this.Command.Values)
             {
-                return;
+                command.Dispose();
             }
-            this.OnDisposing();
-            this.IsDisposed = true;
+            this.Command.Dispose();
+            base.OnDisposing();
         }
 
-        protected virtual void OnDisposing()
+        private class MetaDataPopulatorCommand : BaseComponent
         {
-            foreach (var commands in this.Commands.Values)
-            {
-                commands.Dispose();
-            }
-            this.Commands.Dispose();
-        }
-
-        private class MetaDataPopulatorCommands : BaseComponent
-        {
-            public MetaDataPopulatorCommands(IDatabaseContext databaseContext, IDbTransaction transaction, string prefix)
+            public MetaDataPopulatorCommand(IDatabaseContext databaseContext, IDbTransaction transaction, string prefix)
             {
                 Logger.Write(this, LogLevel.Debug, "Creating meta data populator command set.");
 
                 var metaDataParameters = default(IDbParameterCollection);
 
-                this.MetaDataCommand = databaseContext.Connection.CreateCommand(
+                this.Command = databaseContext.Connection.CreateCommand(
                     string.Format(Resources.AddMetaDataItems, prefix),
                     new[] { "itemId", "name", "type", "numericValue", "textValue", "fileValue" },
                     out metaDataParameters
                 );
 
-                this.MetaDataCommand.Transaction = transaction;
-                this.MetaDataParameters = metaDataParameters;
+                this.Command.Transaction = transaction;
+                this.Parameters = metaDataParameters;
             }
 
-            public IDbCommand MetaDataCommand { get; private set; }
+            public IDbCommand Command { get; private set; }
 
-            public IDbParameterCollection MetaDataParameters { get; private set; }
+            public IDbParameterCollection Parameters { get; private set; }
 
             public bool IsDisposed { get; private set; }
 
@@ -275,7 +144,7 @@ namespace FoxTunes
 
             protected virtual void OnDisposing()
             {
-                this.MetaDataCommand.Dispose();
+                this.Command.Dispose();
             }
         }
     }
---
+++ FoxTunes.Core/Utilities/PopulatorBase.cs
@@ -0,0 +1,145 @@
+﻿using FoxTunes.Interfaces;
+using System;
+
+namespace FoxTunes
+{
+    public abstract class PopulatorBase : BaseComponent, IReportsProgress, IDisposable
+    {
+        private string _Name { get; set; }
+
+        public string Name
+        {
+            get
+            {
+                return this._Name;
+            }
+            protected set
+            {
+                this._Name = value;
+                this.OnNameChanged();
+            }
+        }
+
+        protected virtual void OnNameChanged()
+        {
+            if (this.NameChanged != null)
+            {
+                this.NameChanged(this, EventArgs.Empty);
+            }
+            this.OnPropertyChanged("Name");
+        }
+
+        public event EventHandler NameChanged = delegate { };
+
+        private string _Description { get; set; }
+
+        public string Description
+        {
+            get
+            {
+                return this._Description;
+            }
+            protected set
+            {
+                this._Description = value;
+                this.OnDescriptionChanged();
+            }
+        }
+
+        protected virtual void OnDescriptionChanged()
+        {
+            if (this.DescriptionChanged != null)
+            {
+                this.DescriptionChanged(this, EventArgs.Empty);
+            }
+            this.OnPropertyChanged("Description");
+        }
+
+        public event EventHandler DescriptionChanged = delegate { };
+
+        private int _Position { get; set; }
+
+        public int Position
+        {
+            get
+            {
+                return this._Position;
+            }
+            protected set
+            {
+                this._Position = value;
+                this.OnPositionChanged();
+            }
+        }
+
+        protected virtual void OnPositionChanged()
+        {
+            if (this.PositionChanged != null)
+            {
+                this.PositionChanged(this, EventArgs.Empty);
+            }
+            this.OnPropertyChanged("Position");
+        }
+
+        public event EventHandler PositionChanged = delegate { };
+
+        private int _Count { get; set; }
+
+        public int Count
+        {
+            get
+            {
+                return this._Count;
+            }
+            protected set
+            {
+                this._Count = value;
+                this.OnCountChanged();
+            }
+        }
+
+        protected virtual void OnCountChanged()
+        {
+            if (this.CountChanged != null)
+            {
+                this.CountChanged(this, EventArgs.Empty);
+            }
+            this.OnPropertyChanged("Count");
+        }
+
+        public event EventHandler CountChanged = delegate { };
+
+        public bool IsIndeterminate
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public event EventHandler IsIndeterminateChanged = delegate { };
+
+        public bool IsDisposed { get; private set; }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (this.IsDisposed || !disposing)
+            {
+                return;
+            }
+            this.OnDisposing();
+            this.IsDisposed = true;
+        }
+
+        protected virtual void OnDisposing()
+        {
+            //Nothing to do.
+        }
+    }
+}
