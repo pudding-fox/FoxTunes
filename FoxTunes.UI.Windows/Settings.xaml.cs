1a5c28353ce520c87a73a30ffe325f40ef0df266
aidan <aidan.gilbert@live.co.uk>
2018-12-21 21:52:20 +0000

    Fix settings popup focus issue.
     If settings is open and the user switches to another program,
     the dialog will not work properly until the main window is "focused".
    
     I found this when trying to edit a script and having to use a browser
     for a reference, I could no longer enter text into the textbox.

--- FoxTunes.UI.Windows/Settings.xaml.cs
+++ FoxTunes.UI.Windows/Settings.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace FoxTunes
 {
@@ -10,6 +12,17 @@ namespace FoxTunes
         public Settings()
         {
             this.InitializeComponent();
+        }
+
+        private void OnPreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //If settings is open and the user switches to another program, 
+            //the popup will not work properly until the main window is "focused".
+            var window = this.Parent.FindAncestor<Window>();
+            if (window != null && !window.IsActive)
+            {
+                window.Activate();
+            }
         }
     }
 }
