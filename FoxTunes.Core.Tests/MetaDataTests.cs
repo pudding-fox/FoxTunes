7c20e7093642f3064d8d3e0711cce251963b09f3
aidan <aidan.gilbert@live.co.uk>
2019-01-12 00:58:34 +0000

    Improves File Name Meta Data framework a bit.

--- FoxTunes.Core.Tests/MetaDataTests.cs
+++ FoxTunes.Core.Tests/MetaDataTests.cs
@@ -30,14 +30,24 @@ namespace FoxTunes
             base.TearDown();
         }
 
-        [TestCase(@"C:\Music\Delta Heavy\Paradise Lost\01 Paradise Lost.m4a", "Delta Heavy", "Paradise Lost", null, 1, "Paradise Lost")]
-        [TestCase(@"C:\Music\Compilations\Club Anthems 99\1-01 Moloko - Sing It Back.m4a", "Compilations", "Club Anthems 99", 1, 1, "Moloko - Sing It Back")]
-        [TestCase(@"C:\Music\Compilations\Club Anthems 99\2-09 B.B.E. - Seven Days and One Week '99 (Kai Tracid remix).m4a", "Compilations", "Club Anthems 99", 2, 9, "B.B.E. - Seven Days and One Week '99 (Kai Tracid remix)")]
-        public async Task CanReadMetaData(string fileName, string artist, string album, int? disc, int track, string title)
+        [TestCase(@"C:\Music\Delta Heavy\Paradise Lost\01 Paradise Lost.m4a", "Delta Heavy", null, null, "Paradise Lost", null, 1, "Paradise Lost")]
+        [TestCase(@"C:\Music\Compilations\Club Anthems 99\1-01 Moloko - Sing It Back.m4a", "Compilations", "Moloko", null, "Club Anthems 99", 1, 1, "Sing It Back")]
+        [TestCase(@"C:\Music\Compilations\Club Anthems 99\1-10 Shanks & Bigfoot - Sweet Like Chocolate (Ruff Driverz vocal mix).m4a", "Compilations", "Shanks & Bigfoot", null, "Club Anthems 99", 1, 10, "Sweet Like Chocolate (Ruff Driverz vocal mix)")]
+        [TestCase(@"C:\Music\Compilations\Club Anthems 99\2-09 B.B.E. - Seven Days and One Week '99 (Kai Tracid remix).m4a", "Compilations", "B.B.E.", null, "Club Anthems 99", 2, 9, "Seven Days and One Week '99 (Kai Tracid remix)")]
+        [TestCase(@"C:\Music\Japan\Akira Yamaoka\2006 - iFUTURELIST\04 Tant pis pour toi.m4a", "Akira Yamaoka", null, 2006, "iFUTURELIST", null, 4, "Tant pis pour toi")]
+        public async Task CanReadMetaData(string fileName, string artist, string performer, int? year, string album, int? disc, int track, string title)
         {
             var metaDataSource = this.Core.Factories.MetaDataSource.Create();
             var metaData = (await metaDataSource.GetMetaData(fileName)).ToDictionary(metaDataItem => metaDataItem.Name, metaDataItem => metaDataItem.Value);
             Assert.AreEqual(artist, metaData[CommonMetaData.FirstAlbumArtist]);
+            if (!string.IsNullOrEmpty(performer))
+            {
+                Assert.AreEqual(performer, metaData[CommonMetaData.FirstPerformer]);
+            }
+            if (year.HasValue)
+            {
+                Assert.AreEqual(year.Value, metaData[CommonMetaData.Year]);
+            }
             Assert.AreEqual(album, metaData[CommonMetaData.Album]);
             if (disc.HasValue)
             {
