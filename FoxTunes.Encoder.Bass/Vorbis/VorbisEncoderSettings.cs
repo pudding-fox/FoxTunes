362f663bd47750980919315aeb8b87985e5a19ff
aidan <aidan.gilbert@live.co.uk>
2019-05-03 19:19:21 +0100

    Adds Ogg Vorbis encoder.

---
+++ FoxTunes.Encoder.Bass/Vorbis/VorbisEncoderSettings.cs
@@ -0,0 +1,81 @@
+﻿using FoxTunes.Interfaces;
+using ManagedBass;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FoxTunes
+{
+    public class VorbisEncoderSettings : BassEncoderSettings, IStandardComponent, IConfigurableComponent
+    {
+        public VorbisEncoderSettings()
+        {
+            var directory = Path.GetDirectoryName(
+                typeof(VorbisEncoderSettings).Assembly.Location
+            );
+            this.Executable = Path.Combine(directory, "Encoders\\oggenc2.exe");
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "Ogg Vorbis";
+            }
+        }
+
+        public override string Extension
+        {
+            get
+            {
+                return "ogg";
+            }
+        }
+
+        public override IBassEncoderFormat Format
+        {
+            get
+            {
+                return new BassEncoderFormat(DEPTH_16);
+            }
+        }
+
+        public int Quality { get; private set; }
+
+        public override void InitializeComponent(ICore core)
+        {
+            core.Components.Configuration.GetElement<IntegerConfigurationElement>(
+                VorbisEncoderSettingsConfiguration.SECTION,
+                VorbisEncoderSettingsConfiguration.QUALITY_ELEMENT
+            ).ConnectValue(value => this.Quality = value);
+            base.InitializeComponent(core);
+        }
+
+        public override string GetArguments(EncoderItem encoderItem, IBassStream stream)
+        {
+            var channelInfo = default(ChannelInfo);
+            if (!Bass.ChannelGetInfo(stream.ChannelHandle, out channelInfo))
+            {
+                throw new NotImplementedException();
+            }
+            return string.Format(
+                "--quiet --raw --raw-rate={0} --raw-chan={1} --raw-bits={2} --raw-endianness=0 - --quality={3} -o \"{4}\"",
+                channelInfo.Frequency,
+                channelInfo.Channels,
+                this.GetDepth(encoderItem, stream),
+                this.Quality,
+                encoderItem.OutputFileName
+            );
+        }
+
+        public override int GetDepth(EncoderItem encoderItem, IBassStream stream)
+        {
+            return DEPTH_16;
+        }
+
+        public IEnumerable<ConfigurationSection> GetConfigurationSections()
+        {
+            return VorbisEncoderSettingsConfiguration.GetConfigurationSections(this);
+        }
+    }
+}
