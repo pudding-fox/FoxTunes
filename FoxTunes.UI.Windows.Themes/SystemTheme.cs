pudding-fox/FoxTunes

commit 1aab424f63f4ae0c71813e3eb1b6bcb031ccab64

Allow standard components to be enabled/disabled with CoreSettings.
 Removes the CoreFlags setting as this only allowed for disabling the
 UserInterface. Now any ComponentSlots can be disabled.

 Adds ComponentDependency annotation which prevents components from
 being loaded if a required slot is disabled.

 All this is used mainly to prevent the "encoder" core from
 instantiating unnecessary components and behaviours.

FoxTunes.UI.Windows.Themes/SystemTheme.cs
@@ -5,10 +5,14 @@ using System.Windows;
 
 namespace FoxTunes
 {
+    [Component(ID, ComponentSlots.None, priority: ComponentAttribute.PRIORITY_LOW)]
+    [ComponentDependency(Slot = ComponentSlots.UserInterface)]
     public class SystemTheme : ThemeBase
     {
+        public const string ID = "3E9EFE8C-5245-4F8B-97D1-EB47CC70E373";
+
         public SystemTheme()
-            : base("D4EBB53F-BF59-4D61-99E1-D6D52926AE3F", "System")
+            : base(ID, "System")
         {
 
         }
@@ -35,13 +39,10 @@ namespace FoxTunes
 
         public override void InitializeComponent(ICore core)
         {
-            if (!(core.Flags.HasFlag(CoreFlags.Headless)))
+            this.ResourceDictionary = new ResourceDictionary()
             {
-                this.ResourceDictionary = new ResourceDictionary()
-                {
-                    Source = new Uri("/FoxTunes.UI.Windows.Themes;component/Themes/System.xaml", UriKind.Relative)
-                };
-            }
+                Source = new Uri("/FoxTunes.UI.Windows.Themes;component/Themes/System.xaml", UriKind.Relative)
+            };
             base.InitializeComponent(core);
         }
     }

commit 50124fb4467d6557548f1267e70ac1894dbf161d

Fixes System theme option.
 It has been broken since 1aab424f63f4ae0c71813e3eb1b6bcb031ccab64
 apparently...

FoxTunes.UI.Windows.Themes/SystemTheme.cs
@@ -9,7 +9,7 @@ namespace FoxTunes
     [ComponentDependency(Slot = ComponentSlots.UserInterface)]
     public class SystemTheme : ThemeBase
     {
-        public const string ID = "3E9EFE8C-5245-4F8B-97D1-EB47CC70E373";
+        public const string ID = "392B2FEE-B1E5-4776-AF3A-C28260EE8E81";
 
         public SystemTheme()
             : base(ID, "System")