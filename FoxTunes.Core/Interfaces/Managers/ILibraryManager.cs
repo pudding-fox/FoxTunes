73a21d24acfaecc0d332a34bc13d9c60f84b887e
aidan <aidan.gilbert@live.co.uk>
2020-01-12 12:56:20 +0000

    Adds reporting to librayr manager.

--- FoxTunes.Core/Interfaces/Managers/ILibraryManager.cs
+++ FoxTunes.Core/Interfaces/Managers/ILibraryManager.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 
 namespace FoxTunes.Interfaces
 {
-    public interface ILibraryManager : IStandardManager, IBackgroundTaskSource
+    public interface ILibraryManager : IStandardManager, IBackgroundTaskSource, IReportSource
     {
         LibraryManagerState State { get; }
 
--- FoxTunes.Core/Managers/LibraryManager.cs
+++ FoxTunes.Core/Managers/LibraryManager.cs
@@ -1,4 +1,5 @@
-﻿using FoxDb;
+﻿#pragma warning disable 612, 618
+using FoxDb;
 using FoxDb.Interfaces;
 using FoxTunes.Interfaces;
 using System;
@@ -227,6 +228,7 @@ namespace FoxTunes
                 task.InitializeComponent(this.Core);
                 await this.OnBackgroundTask(task).ConfigureAwait(false);
                 await task.Run().ConfigureAwait(false);
+                this.OnReport();
             }
         }
 
@@ -247,6 +249,7 @@ namespace FoxTunes
                 task.InitializeComponent(this.Core);
                 await this.OnBackgroundTask(task).ConfigureAwait(false);
                 await task.Run().ConfigureAwait(false);
+                this.OnReport();
             }
         }
 
@@ -257,6 +260,7 @@ namespace FoxTunes
                 task.InitializeComponent(this.Core);
                 await this.OnBackgroundTask(task).ConfigureAwait(false);
                 await task.Run().ConfigureAwait(false);
+                this.OnReport();
             }
         }
 
@@ -326,5 +330,145 @@ namespace FoxTunes
         }
 
         public event BackgroundTaskEventHandler BackgroundTask;
+
+        protected virtual void OnReport()
+        {
+            var report = new LibraryManagerReport(this.DatabaseFactory);
+            report.InitializeComponent(this.Core);
+            this.OnReport(report);
+        }
+
+        protected virtual Task OnReport(IReport Report)
+        {
+            if (this.Report == null)
+            {
+#if NET40
+                return TaskEx.FromResult(false);
+#else
+                return Task.CompletedTask;
+#endif
+            }
+            var e = new ReportEventArgs(Report);
+            this.Report(this, e);
+            return e.Complete();
+        }
+
+        public event ReportEventHandler Report;
+
+        public class LibraryManagerReport : BaseComponent, IReport
+        {
+            public LibraryManagerReport(IDatabaseFactory databaseFactory)
+            {
+                this.DatabaseFactory = databaseFactory;
+            }
+
+            public IDatabaseFactory DatabaseFactory { get; private set; }
+
+            public string Title
+            {
+                get
+                {
+                    return "Library Status";
+                }
+            }
+
+            public string Description
+            {
+                get
+                {
+                    return string.Empty;
+                }
+            }
+
+            public string[] Headers
+            {
+                get
+                {
+                    return new[]
+                    {
+                        "Path",
+                        "Tracks"
+                    };
+                }
+            }
+
+            public IEnumerable<IReportRow> Rows
+            {
+                get
+                {
+                    using (var database = this.DatabaseFactory.Create())
+                    {
+                        using (var transaction = database.BeginTransaction(database.PreferredIsolationLevel))
+                        {
+                            var libraryRoots = database.Set<LibraryRoot>(transaction);
+                            foreach (var libraryRoot in libraryRoots)
+                            {
+                                yield return this.GetRow(database, libraryRoot, transaction);
+                            }
+                        }
+                    }
+                }
+            }
+
+            protected virtual IReportRow GetRow(IDatabaseComponent database, LibraryRoot libraryRoot, ITransactionSource transaction)
+            {
+                var table = database.Tables.LibraryItem;
+                var builder = database.QueryFactory.Build();
+                builder.Output.AddFunction(QueryFunction.Count, builder.Output.CreateOperator(QueryOperator.Star));
+                builder.Source.AddTable(table);
+                builder.Filter.Add().With(binary =>
+                {
+                    binary.Left = binary.CreateColumn(table.Column("DirectoryName"));
+                    binary.Operator = binary.CreateOperator(QueryOperator.Match);
+                    binary.Right = binary.CreateConstant(libraryRoot.DirectoryName + "%");
+                });
+                var count = database.ExecuteScalar<long>(builder.Build(), transaction);
+                return new ReportRow(libraryRoot.DirectoryName, count);
+            }
+
+            public Action<Guid> Action
+            {
+                get
+                {
+                    return key =>
+                    {
+                        //Nothing to do.
+                    };
+                }
+            }
+
+            private class ReportRow : IReportRow
+            {
+                public ReportRow(string directoryName, long count)
+                {
+                    this.DirectoryName = directoryName;
+                    this.Count = count;
+                }
+
+                public string DirectoryName { get; private set; }
+
+                public long Count { get; private set; }
+
+                public Guid Id
+                {
+                    get
+                    {
+                        return Guid.Empty;
+                    }
+                }
+
+                public string[] Values
+                {
+                    get
+                    {
+                        return new[]
+                        {
+                            this.DirectoryName,
+                            Convert.ToString(this.Count)
+                        };
+                    }
+                }
+            }
+        }
     }
 }
