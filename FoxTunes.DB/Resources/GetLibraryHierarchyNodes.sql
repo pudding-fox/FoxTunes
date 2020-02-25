456583fe9b875bfcb21bc7f1a44f7a9583ed1487
aidan <aidan.gilbert@live.co.uk>
2020-02-25 21:03:59 +0000

    Moves library hierarchy filtering logic to new template.

--- FoxTunes.DB/Resources/GetLibraryHierarchyNodes.sql
+++
@@ -1,6 +0,0 @@
-﻿SELECT "LibraryHierarchyItems"."Id", "LibraryHierarchy_Id", "Value", "IsLeaf"
-FROM "LibraryHierarchyItems"
-WHERE "LibraryHierarchy_Id" = @libraryHierarchyId
-	AND ((@libraryHierarchyItemId IS NULL AND "LibraryHierarchyItems"."Parent_Id" IS NULL) 
-		OR "LibraryHierarchyItems"."Parent_Id" = @libraryHierarchyItemId)
-ORDER BY "Value"
