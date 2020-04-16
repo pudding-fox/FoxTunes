c0e19adf359ffd252b2a4fe9e2b855c9369e085f
aidan <aidan.gilbert@live.co.uk>
2020-04-16 11:51:50 +0100

    Improve replay gain scanner progress reporting.
     Monitor stream positions to determine scanner progress.

---
+++ FoxTunes.Output.Bass.ReplayGain/ChannelMonitor.cs
@@ -0,0 +1,120 @@
+﻿using FoxTunes.Interfaces;
+using ManagedBass;
+using System;
+using System.Timers;
+
+namespace FoxTunes
+{
+    public class ChannelMonitor : BaseComponent, IDisposable
+    {
+        public static readonly int INTERVAL = 1000;
+
+        public static readonly object SyncRoot = new object();
+
+        public ChannelMonitor(ScannerItem scannerItem, IBassStream stream)
+        {
+            this.ScannerItem = scannerItem;
+            this.Stream = stream;
+        }
+
+        public ScannerItem ScannerItem { get; private set; }
+
+        public IBassStream Stream { get; private set; }
+
+        public global::System.Timers.Timer Timer { get; private set; }
+
+        public void Start()
+        {
+            lock (SyncRoot)
+            {
+                if (this.Timer == null)
+                {
+                    this.Timer = new global::System.Timers.Timer();
+                    this.Timer.Interval = INTERVAL;
+                    this.Timer.AutoReset = false;
+                    this.Timer.Elapsed += this.OnElapsed;
+                    this.Timer.Start();
+                }
+            }
+        }
+
+        public void Stop()
+        {
+            lock (SyncRoot)
+            {
+                if (this.Timer != null)
+                {
+                    this.Timer.Stop();
+                    this.Timer.Elapsed -= this.OnElapsed;
+                    this.Timer.Dispose();
+                    this.Timer = null;
+                }
+            }
+        }
+
+        protected virtual void Update()
+        {
+            var length = Bass.ChannelGetLength(this.Stream.ChannelHandle);
+            var position = Bass.ChannelGetPosition(this.Stream.ChannelHandle);
+            if (position == length)
+            {
+                this.ScannerItem.Progress = ScannerItem.PROGRESS_COMPLETE;
+                this.ScannerItem.Status = ScannerItemStatus.Complete;
+            }
+            else
+            {
+                this.ScannerItem.Progress = (int)(((float)position / length) * 100);
+                this.ScannerItem.Status = ScannerItemStatus.Processing;
+            }
+        }
+
+        protected virtual void OnElapsed(object sender, ElapsedEventArgs e)
+        {
+            this.Update();
+            lock (SyncRoot)
+            {
+                if (this.Timer == null)
+                {
+                    return;
+                }
+                this.Timer.Start();
+            }
+        }
+
+        public bool IsDisposed { get; private set; }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (this.IsDisposed || !disposing)
+            {
+                return;
+            }
+            this.OnDisposing();
+            this.IsDisposed = true;
+        }
+
+        protected virtual void OnDisposing()
+        {
+            this.Stop();
+        }
+
+        ~ChannelMonitor()
+        {
+            Logger.Write(this, LogLevel.Error, "Component was not disposed: {0}", this.GetType().Name);
+            try
+            {
+                this.Dispose(true);
+            }
+            catch
+            {
+                //Nothing can be done, never throw on GC thread.
+            }
+        }
+    }
+}
