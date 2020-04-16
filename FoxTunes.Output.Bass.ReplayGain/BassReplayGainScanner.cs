pudding-fox/FoxTunes

commit c0e19adf359ffd252b2a4fe9e2b855c9369e085f

Improve replay gain scanner progress reporting.
 Monitor stream positions to determine scanner progress.

FoxTunes.Output.Bass.ReplayGain/BassReplayGainScanner.cs
@@ -151,12 +151,15 @@ namespace FoxTunes
                 throw new InvalidOperationException(string.Format("Failed to create stream for file \"{0}\": Unknown error.", scannerItem.FileName));
             }
             Logger.Write(this, LogLevel.Debug, "Created stream for file \"{0}\": {1}", scannerItem.FileName, stream.ChannelHandle);
+            var monitor = new ChannelMonitor(scannerItem, stream);
             try
             {
+                monitor.Start();
                 success = this.ScanTrack(scannerItem, stream);
             }
             finally
             {
+                monitor.Dispose();
                 Logger.Write(this, LogLevel.Debug, "Releasing stream for file \"{0}\": {1}", scannerItem.FileName, stream.ChannelHandle);
                 Bass.StreamFree(stream.ChannelHandle);
             }
@@ -238,12 +241,24 @@ namespace FoxTunes
                     }
                 }
                 var success = default(bool);
+                var monitors = new List<ChannelMonitor>();
                 try
                 {
+                    foreach (var scannerItem in streams.Keys)
+                    {
+                        var stream = streams[scannerItem];
+                        var monitor = new ChannelMonitor(scannerItem, stream);
+                        monitor.Start();
+                        monitors.Add(monitor);
+                    }
                     success = this.ScanGroup(streams);
                 }
                 finally
                 {
+                    foreach (var monitor in monitors)
+                    {
+                        monitor.Dispose();
+                    }
                     foreach (var scannerItem in streams.Keys)
                     {
                         var stream = streams[scannerItem];

FoxTunes.Output.Bass.ReplayGain/ChannelMonitor.cs
@@ -1,0 +1,120 @@
+﻿using FoxTunes.Interfaces;
+using ManagedBass;
+using System;
+using System.Timers;
+
+namespace FoxTunes
+{
+    public class ChannelMonitor : BaseComponent, IDisposable
+    {
+        public static readonly int INTERVAL = 1000;
+
+        public static readonly object SyncRoot = new object();
+
+        public ChannelMonitor(ScannerItem scannerItem, IBassStream stream)
+        {
+            this.ScannerItem = scannerItem;
+            this.Stream = stream;
+        }
+
+        public ScannerItem ScannerItem { get; private set; }
+
+        public IBassStream Stream { get; private set; }
+
+        public global::System.Timers.Timer Timer { get; private set; }
+
+        public void Start()
+        {
+            lock (SyncRoot)
+            {
+                if (this.Timer == null)
+                {
+                    this.Timer = new global::System.Timers.Timer();
+                    this.Timer.Interval = INTERVAL;
+                    this.Timer.AutoReset = false;
+                    this.Timer.Elapsed += this.OnElapsed;
+                    this.Timer.Start();
+                }
+            }
+        }
+
+        public void Stop()
+        {
+            lock (SyncRoot)
+            {
+                if (this.Timer != null)
+                {
+                    this.Timer.Stop();
+                    this.Timer.Elapsed -= this.OnElapsed;
+                    this.Timer.Dispose();
+                    this.Timer = null;
+                }
+            }
+        }
+
+        protected virtual void Update()
+        {
+            var length = Bass.ChannelGetLength(this.Stream.ChannelHandle);
+            var position = Bass.ChannelGetPosition(this.Stream.ChannelHandle);
+            if (position == length)
+            {
+                this.ScannerItem.Progress = ScannerItem.PROGRESS_COMPLETE;
+                this.ScannerItem.Status = ScannerItemStatus.Complete;
+            }
+            else
+            {
+                this.ScannerItem.Progress = (int)(((float)position / length) * 100);
+                this.ScannerItem.Status = ScannerItemStatus.Processing;
+            }
+        }
+
+        protected virtual void OnElapsed(object sender, ElapsedEventArgs e)
+        {
+            this.Update();
+            lock (SyncRoot)
+            {
+                if (this.Timer == null)
+                {
+                    return;
+                }
+                this.Timer.Start();
+            }
+        }
+
+        public bool IsDisposed { get; private set; }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (this.IsDisposed || !disposing)
+            {
+                return;
+            }
+            this.OnDisposing();
+            this.IsDisposed = true;
+        }
+
+        protected virtual void OnDisposing()
+        {
+            this.Stop();
+        }
+
+        ~ChannelMonitor()
+        {
+            Logger.Write(this, LogLevel.Error, "Component was not disposed: {0}", this.GetType().Name);
+            try
+            {
+                this.Dispose(true);
+            }
+            catch
+            {
+                //Nothing can be done, never throw on GC thread.
+            }
+        }
+    }
+}

commit 5ea7313a40ae8bd009b52b1ad02a56b6b117be2c

Fix replay gain data isn't updated for albums.
 Regression caused by c0e19adf359ffd252b2a4fe9e2b855c9369e085f.

FoxTunes.Output.Bass.ReplayGain/BassReplayGainScanner.cs
@@ -303,10 +303,6 @@ namespace FoxTunes
             }
             foreach (var scannerItem in group.Keys)
             {
-                if (scannerItem.Status != ScannerItemStatus.Processing)
-                {
-                    continue;
-                }
                 var stream = group[scannerItem];
                 foreach (var item in info.items)
                 {

FoxTunes.Output.Bass.ReplayGain/ChannelMonitor.cs
@@ -7,7 +7,7 @@ namespace FoxTunes
 {
     public class ChannelMonitor : BaseComponent, IDisposable
     {
-        public static readonly int INTERVAL = 1000;
+        public static readonly int INTERVAL = 500;
 
         public static readonly object SyncRoot = new object();
 
@@ -59,7 +59,7 @@ namespace FoxTunes
             if (position == length)
             {
                 this.ScannerItem.Progress = ScannerItem.PROGRESS_COMPLETE;
-                this.ScannerItem.Status = ScannerItemStatus.Complete;
+                this.Stop();
             }
             else
             {