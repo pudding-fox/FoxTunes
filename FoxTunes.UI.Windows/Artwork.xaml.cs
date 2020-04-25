1fb70d5bcb7a5809c5aeceebb2aab5f1e1b54442
aidan <aidan.gilbert@live.co.uk>
2020-04-25 14:11:16 +0100

    Fix artwork does not resize in Info view.

--- FoxTunes.UI.Windows/Artwork.xaml.cs
+++ FoxTunes.UI.Windows/Artwork.xaml.cs
@@ -3,8 +3,6 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Media;
 
 namespace FoxTunes
@@ -14,7 +12,7 @@ namespace FoxTunes
     /// </summary>
     [UIComponent("66C8A9E7-0891-48DD-8086-E40F72D4D030", UIComponentSlots.NONE, "Artwork")]
     [UIComponentDependency(MetaDataBehaviourConfiguration.SECTION, MetaDataBehaviourConfiguration.ENABLE_ELEMENT)]
-    public partial class Artwork : UIComponentBase
+    public partial class Artwork : SquareUIComponentBase
     {
         public static readonly IArtworkProvider ArtworkProvider = ComponentRegistry.Instance.GetComponent<IArtworkProvider>();
 
@@ -110,67 +108,6 @@ namespace FoxTunes
                     this.IsComponentEnabled = true;
                 }).ConfigureAwait(false);
             }
-        }
-
-        protected override void OnVisualParentChanged(DependencyObject oldParent)
-        {
-            if (this.Parent != null)
-            {
-                var row = default(RowDefinition);
-                var grid = this.Parent.FindAncestor<Grid>();
-                if (grid != null)
-                {
-                    var index = Grid.GetRow(this);
-                    if (index < grid.RowDefinitions.Count)
-                    {
-                        row = grid.RowDefinitions[index];
-                        BindingHelper.AddHandler(row, RowDefinition.HeightProperty, typeof(RowDefinition), (sender, e) =>
-                        {
-                            this.UpdateLayoutSource(row);
-                        });
-                    }
-                }
-                this.UpdateLayoutSource(row);
-            }
-            base.OnVisualParentChanged(oldParent);
-        }
-
-        protected virtual void UpdateLayoutSource(RowDefinition row = null)
-        {
-            if (row == null || row.Height.IsAuto || row.Height.IsStar)
-            {
-                this.SizeChanged += this.OnSizeChanged;
-            }
-            else
-            {
-                BindingOperations.ClearBinding(this, WidthProperty);
-                BindingOperations.ClearBinding(this, HeightProperty);
-            }
-        }
-
-        protected virtual void OnSizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            BindingOperations.ClearBinding(this, WidthProperty);
-            BindingOperations.ClearBinding(this, HeightProperty);
-            if (this.ActualWidth > 0)
-            {
-                BindingOperations.SetBinding(this, HeightProperty, new Binding("ActualWidth")
-                {
-                    RelativeSource = new RelativeSource(RelativeSourceMode.Self)
-                });
-            }
-            else if (this.ActualHeight > 0)
-            {
-                BindingOperations.SetBinding(this, WidthProperty, new Binding("ActualHeight")
-                {
-                    RelativeSource = new RelativeSource(RelativeSourceMode.Self)
-                });
-            }
-            else
-            {
-                return;
-            }
-            this.SizeChanged -= this.OnSizeChanged;
         }
     }
 }
