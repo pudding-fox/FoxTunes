44593421b76736a157ebe659ac680d1a6f5a7deb
aidan <aidan.gilbert@live.co.uk>
2020-04-30 15:49:15 +0100

    Moves spectum border back where it was.
     Forgot that this control could be disabled, the border needs to be a
     child so it's not visible.

--- FoxTunes.UI.Windows/Spectrum.xaml.cs
+++ FoxTunes.UI.Windows/Spectrum.xaml.cs
@@ -12,18 +12,6 @@ namespace FoxTunes
     /// </summary>
     public partial class Spectrum : UserControl
     {
-        protected static ILogger Logger
-        {
-            get
-            {
-                return LogManager.Logger;
-            }
-        }
-
-        const int FACTOR = 4;
-
-        const int UPDATE_INTERVAL = 100;
-
         public static readonly IOutput Output = ComponentRegistry.Instance.GetComponent<IOutput>();
 
         public static readonly BooleanConfigurationElement Enabled;
@@ -47,33 +35,11 @@ namespace FoxTunes
             );
         }
 
-        public static readonly int SampleCount = 512;
-
-        public static readonly float[] Buffer = new float[SampleCount];
-
-        public int[] Dimentions = new int[2];
-
-        public int Step;
-
-        public int ElementCount;
-
-        public int[,] Elements;
-
-        public int[,] Peaks;
-
-        public int SamplesPerElement;
-
-        public float Weight;
-
-        public bool HasData = false;
-
         public Spectrum()
         {
             this.InitializeComponent();
-            this.Timer = new Timer();
-            this.Timer.Interval = UPDATE_INTERVAL;
-            this.Timer.AutoReset = false;
-            this.Timer.Elapsed += this.OnElapsed;
+            var renderer = new Renderer();
+            this.Border.Child = renderer;
             if (Enabled != null)
             {
                 Enabled.ConnectValue(value =>
@@ -81,150 +47,221 @@ namespace FoxTunes
                     if (value)
                     {
                         this.Visibility = Visibility.Visible;
-                        this.Timer.Start();
+                        renderer.Start();
                     }
                     else
                     {
                         this.Visibility = Visibility.Collapsed;
-                        this.Timer.Stop();
+                        renderer.Stop();
                     }
                 });
             }
-            if (Bars != null)
-            {
-                Bars.ConnectValue(value =>
-                    this.Configure(
-                        SpectrumBehaviourConfiguration.GetBars(value),
-                        SpectrumBehaviourConfiguration.GetWidth(value)
-                    )
-                );
-            }
         }
 
-        public Timer Timer { get; private set; }
-
-        protected virtual void OnSizeChanged(object sender, SizeChangedEventArgs e)
+        private class Renderer : Control
         {
-            this.Dimentions[0] = Convert.ToInt32(this.ActualWidth);
-            this.Dimentions[1] = Convert.ToInt32(this.ActualHeight);
-        }
+            public static readonly object SyncRoot = new object();
 
-        protected override void OnRender(DrawingContext drawingContext)
-        {
-            base.OnRender(drawingContext);
-            if (this.HasData)
+            protected static ILogger Logger
             {
-                var brush = this.Foreground;
-                var pen = new Pen(this.Foreground, 0.4);
-                for (var a = 0; a < ElementCount; a++)
+                get
                 {
-                    drawingContext.DrawRectangle(
-                        brush,
-                        pen,
-                        new Rect(
-                            this.Elements[a, 0],
-                            this.Elements[a, 1],
-                            this.Elements[a, 2],
-                            this.Elements[a, 3]
+                    return LogManager.Logger;
+                }
+            }
+
+            const int FACTOR = 4;
+
+            const int UPDATE_INTERVAL = 100;
+
+            public static readonly int SampleCount = 512;
+
+            public static readonly float[] Buffer = new float[SampleCount];
+
+            public int[] Dimentions = new int[2];
+
+            public int Step;
+
+            public int ElementCount;
+
+            public int[,] Elements;
+
+            public int[,] Peaks;
+
+            public int SamplesPerElement;
+
+            public float Weight;
+
+            public bool HasData = false;
+
+            public Renderer()
+            {
+                if (Bars != null)
+                {
+                    Bars.ConnectValue(value =>
+                        this.Configure(
+                            SpectrumBehaviourConfiguration.GetBars(value),
+                            SpectrumBehaviourConfiguration.GetWidth(value)
                         )
                     );
-                    if (this.Elements[a, 1] > this.Peaks[a, 1])
+                }
+            }
+
+            public Timer Timer { get; private set; }
+
+            public void Start()
+            {
+                lock (SyncRoot)
+                {
+                    if (this.Timer == null)
                     {
-                        drawingContext.DrawRectangle(
-                        brush,
-                        pen,
-                        new Rect(
-                            this.Peaks[a, 0],
-                            this.Peaks[a, 1],
-                            this.Peaks[a, 2],
-                            this.Peaks[a, 3]
-                        )
-                    );
+                        this.Timer = new Timer();
+                        this.Timer.Interval = UPDATE_INTERVAL;
+                        this.Timer.AutoReset = false;
+                        this.Timer.Elapsed += this.OnElapsed;
+                        this.Timer.Start();
                     }
                 }
             }
-            this.Timer.Start();
-        }
 
-        protected virtual void OnElapsed(object sender, ElapsedEventArgs e)
-        {
-            try
+            public void Stop()
             {
-                var length = Output.GetData(Buffer);
-                if (length <= 0)
+                lock (SyncRoot)
                 {
-                    this.HasData = false;
-                }
-                else
-                {
-                    Update(this.ElementCount, this.SamplesPerElement, this.Weight, this.Step, this.Dimentions[1], this.Elements, this.Peaks);
-                    this.HasData = true;
-                }
-                Windows.Invoke(() => this.InvalidateVisual());
-            }
-            catch (Exception exception)
-            {
-                Logger.Write(this.GetType(), LogLevel.Warn, "Failed to update spectrum data, disabling: {0}", exception.Message);
-            }
-        }
-
-        protected virtual void Configure(int count, int width)
-        {
-            Windows.Invoke(() => this.Width = width);
-            this.ElementCount = count;
-            this.Elements = new int[count, 4];
-            this.Peaks = new int[count, 4];
-            this.SamplesPerElement = SampleCount / count;
-            this.Weight = (float)16 / this.ElementCount;
-            this.Step = width / ElementCount;
-        }
-
-        private static void Update(int count, int samples, float weight, int step, int height, int[,] elements, int[,] peaks)
-        {
-            for (int a = 0, b = 0; a < count; a++)
-            {
-                var sample = 0f;
-                for (var c = 0; c < samples; b++, c++)
-                {
-                    sample += Buffer[b];
-                }
-                sample /= samples;
-                var factor = FACTOR + (a * weight);
-                var value = Math.Sqrt(sample) * factor;
-                if (value > 1)
-                {
-                    value = 1;
-                }
-                else if (value < 0)
-                {
-                    value = 0;
-                }
-                elements[a, 0] = a * step;
-                elements[a, 2] = step;
-                elements[a, 3] = Convert.ToInt32(value * height);
-                elements[a, 1] = height - elements[a, 3];
-                if (elements[a, 1] < peaks[a, 1])
-                {
-                    peaks[a, 0] = a * step;
-                    peaks[a, 2] = step;
-                    peaks[a, 3] = 1;
-                    peaks[a, 1] = elements[a, 1];
-                }
-                else if (peaks[a, 1] < height - 1)
-                {
-                    peaks[a, 1]++;
+                    if (this.Timer != null)
+                    {
+                        this.Timer.Stop();
+                        this.Timer.Elapsed -= this.OnElapsed;
+                        this.Timer.Dispose();
+                        this.Timer = null;
+                    }
                 }
             }
-        }
 
-        ~Spectrum()
-        {
-            if (this.Timer != null)
+            protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
             {
-                this.Timer.Stop();
-                this.Timer.Elapsed -= this.OnElapsed;
-                this.Timer.Dispose();
-                this.Timer = null;
+                base.OnRenderSizeChanged(sizeInfo);
+                this.Dimentions[0] = Convert.ToInt32(this.ActualWidth);
+                this.Dimentions[1] = Convert.ToInt32(this.ActualHeight);
+            }
+
+            protected override void OnRender(DrawingContext drawingContext)
+            {
+                base.OnRender(drawingContext);
+                if (this.HasData)
+                {
+                    var brush = this.Foreground;
+                    var pen = new Pen(this.Foreground, 0.4);
+                    for (var a = 0; a < ElementCount; a++)
+                    {
+                        drawingContext.DrawRectangle(
+                            brush,
+                            pen,
+                            new Rect(
+                                this.Elements[a, 0],
+                                this.Elements[a, 1],
+                                this.Elements[a, 2],
+                                this.Elements[a, 3]
+                            )
+                        );
+                        if (this.Elements[a, 1] > this.Peaks[a, 1])
+                        {
+                            drawingContext.DrawRectangle(
+                            brush,
+                            pen,
+                            new Rect(
+                                this.Peaks[a, 0],
+                                this.Peaks[a, 1],
+                                this.Peaks[a, 2],
+                                this.Peaks[a, 3]
+                            )
+                        );
+                        }
+                    }
+                }
+                this.Timer.Start();
+            }
+
+            protected virtual void OnElapsed(object sender, ElapsedEventArgs e)
+            {
+                try
+                {
+                    var length = Output.GetData(Buffer);
+                    if (length <= 0)
+                    {
+                        this.HasData = false;
+                    }
+                    else
+                    {
+                        Update(this.ElementCount, this.SamplesPerElement, this.Weight, this.Step, this.Dimentions[1], this.Elements, this.Peaks);
+                        this.HasData = true;
+                    }
+                    Windows.Invoke(() => this.InvalidateVisual());
+                }
+                catch (Exception exception)
+                {
+                    Logger.Write(this.GetType(), LogLevel.Warn, "Failed to update spectrum data, disabling: {0}", exception.Message);
+                }
+            }
+
+            protected virtual void Configure(int count, int width)
+            {
+                Windows.Invoke(() => this.Width = width);
+                this.ElementCount = count;
+                this.Elements = new int[count, 4];
+                this.Peaks = new int[count, 4];
+                this.SamplesPerElement = SampleCount / count;
+                this.Weight = (float)16 / this.ElementCount;
+                this.Step = width / ElementCount;
+            }
+
+            private static void Update(int count, int samples, float weight, int step, int height, int[,] elements, int[,] peaks)
+            {
+                for (int a = 0, b = 0; a < count; a++)
+                {
+                    var sample = 0f;
+                    for (var c = 0; c < samples; b++, c++)
+                    {
+                        sample += Buffer[b];
+                    }
+                    sample /= samples;
+                    var factor = FACTOR + (a * weight);
+                    var value = Math.Sqrt(sample) * factor;
+                    if (value > 1)
+                    {
+                        value = 1;
+                    }
+                    else if (value < 0)
+                    {
+                        value = 0;
+                    }
+                    elements[a, 0] = a * step;
+                    elements[a, 2] = step;
+                    elements[a, 3] = Convert.ToInt32(value * height);
+                    elements[a, 1] = height - elements[a, 3];
+                    if (elements[a, 1] < peaks[a, 1])
+                    {
+                        peaks[a, 0] = a * step;
+                        peaks[a, 2] = step;
+                        peaks[a, 3] = 1;
+                        peaks[a, 1] = elements[a, 1];
+                    }
+                    else if (peaks[a, 1] < height - 1)
+                    {
+                        peaks[a, 1]++;
+                    }
+                }
+            }
+
+            ~Renderer()
+            {
+                if (this.Timer != null)
+                {
+                    this.Timer.Stop();
+                    this.Timer.Elapsed -= this.OnElapsed;
+                    this.Timer.Dispose();
+                    this.Timer = null;
+                }
             }
         }
     }
