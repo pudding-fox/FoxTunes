0cb80b40bc43edbbfc6c450fdb38a3c9bd05f96d
aidan <aidan.gilbert@live.co.uk>
2020-06-14 23:09:41 +0100

    Always create stream components.
     They might be toggled while playback is active.
    
     This is reverting changes in e8b5e0a4ebc28ee21b931f9f8b331bbb72104d57
     that introduced a regression where eq cannot be enabled while playing.

--- FoxTunes.Output.Bass.ReplayGain/BassReplayGainBehaviour.cs
+++ FoxTunes.Output.Bass.ReplayGain/BassReplayGainBehaviour.cs
@@ -110,10 +110,6 @@ namespace FoxTunes
 
         protected virtual void OnCreatingPipeline(object sender, CreatingPipelineEventArgs e)
         {
-            if (!this.Enabled)
-            {
-                return;
-            }
             if (BassUtils.GetChannelDsdRaw(e.Stream.ChannelHandle))
             {
                 //Cannot apply effects to DSD.
