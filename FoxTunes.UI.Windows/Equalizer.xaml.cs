a791989deeaf4c51485ccc3047d4b57834120d66
Aidan Gilbert <aidan.gilbert@live.co.uk>
2020-07-20 22:42:08 +0100

    Some more components can be hosted in tool windows.

--- FoxTunes.UI.Windows/Equalizer.xaml.cs
+++ FoxTunes.UI.Windows/Equalizer.xaml.cs
@@ -1,11 +1,10 @@
-﻿using System.Windows.Controls;
-
-namespace FoxTunes
+﻿namespace FoxTunes
 {
     /// <summary>
     /// Interaction logic for Equalizer.xaml
     /// </summary>
-    public partial class Equalizer : UserControl
+    [UIComponent("F0398311-BF56-4307-B6A8-E30F283F7C89", UIComponentSlots.NONE, "Equalizer Launcher", role: UIComponentRole.Hidden)]
+    public partial class Equalizer : UIComponentBase
     {
         public Equalizer()
         {
