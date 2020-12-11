0ef9b676cba92b04da65cdee014a72912723f3e8
Aidan Gilbert <aidan.gilbert@live.co.uk>
2020-12-11 22:40:11 +0000

    Adds setting for device monitor.
     Enabled by default on normal release.
     Also implements minimum platform check before loading behaviours.

---
+++ FoxTunes.Core/PlatformDependencyAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FoxTunes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+    public class PlatformDependencyAttribute : Attribute
+    {
+        public PlatformDependencyAttribute()
+        {
+
+        }
+
+        public int Major { get; set; }
+
+        public int Minor { get; set; }
+    }
+}
