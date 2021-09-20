1f649a71a211a3884d726579424c42ad5cf7b65c
aidan <aidan.gilbert@live.co.uk>
2021-09-20 16:24:09 +0100

    Ensure visualization config is available at startup.
     Other components read the profile at startup so we need to make sure
     it's loaded first.

--- FoxTunes.UI.Windows/Behaviours/VisualizationBehaviour.cs
+++ FoxTunes.UI.Windows/Behaviours/VisualizationBehaviour.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 
 namespace FoxTunes
 {
+    //PRIORITY_HIGH: Other components inspect our configuration on startup so make sure it's available.
+    [Component("07E05BB1-EF74-43E2-9582-1EE609EBDD10", ComponentSlots.None, priority: ComponentAttribute.PRIORITY_HIGH)]
     public class VisualizationBehaviour : StandardBehaviour, IConfigurableComponent
     {
         public IEnumerable<ConfigurationSection> GetConfigurationSections()
