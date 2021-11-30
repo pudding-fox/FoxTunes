2cb65def4c1dd1e3634ef4077e62912903b009b5
aidan <aidan.gilbert@live.co.uk>
2021-11-30 14:54:22 +0000

    Fix regression: Meta data that isn't cached is never written.
     Oh dear, someone did not understand the change in a9a7cd5761038ecb5d999cda708dc7ea8a47b489

---
+++ FoxTunes.Core/Utilities/PredicatedCache.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FoxTunes
+{
+    public abstract class PredicatedCache<TKey, TValue>
+    {
+        protected PredicatedCache(int capacity)
+        {
+            this.Store = new CappedDictionary<TKey, TValue>(capacity);
+        }
+
+        public CappedDictionary<TKey, TValue> Store { get; private set; }
+
+        public TValue GetOrAdd(TKey key, Func<TValue> factory)
+        {
+            if (!this.CanCache(key))
+            {
+                return factory();
+            }
+            return this.Store.GetOrAdd(key, factory);
+        }
+
+        protected abstract bool CanCache(TKey key);
+    }
+}
