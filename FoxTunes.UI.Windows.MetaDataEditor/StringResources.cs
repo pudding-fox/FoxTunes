2f697b6644b8e443f8368df7818b9a7b38707733
aidan <aidan.gilbert@live.co.uk>
2022-02-02 13:00:47 +0000

    Fix meta data editor always shows <Multiple Values>.

---
+++ FoxTunes.UI.Windows.MetaDataEditor/StringResources.cs
@@ -0,0 +1,25 @@
+﻿namespace FoxTunes
+{
+    /// <summary>
+    /// This class just exposes the internal properties of <see cref="Strings"/> so we don't need to use PublicResXFileCodeGenerator.
+    /// PublicResXFileCodeGenerator causes many type import conflict warnings.
+    /// </summary>
+    public static class StringResources
+    {
+        public static string MetaDataEntry_NoValue
+        {
+            get
+            {
+                return Strings.MetaDataEntry_NoValue;
+            }
+        }
+
+        public static string MetaDataEntry_MultipleValues
+        {
+            get
+            {
+                return Strings.MetaDataEntry_MultipleValues;
+            }
+        }
+    }
+}
