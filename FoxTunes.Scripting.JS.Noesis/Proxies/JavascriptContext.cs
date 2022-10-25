388b7d5e7c29bf77483ee566d4d4193c18e03994
aidan <aidan.gilbert@live.co.uk>
2022-10-25 15:24:25 +0100

    Fix Noesis.Javascript on x64

---
+++ FoxTunes.Scripting.JS.Noesis/Proxies/JavascriptContext.cs
@@ -0,0 +1,96 @@
+﻿using FoxTunes.Interfaces;
+using System;
+
+namespace FoxTunes.Proxies
+{
+    public class JavascriptContext : IDisposable
+    {
+        protected static ILogger Logger
+        {
+            get
+            {
+                return LogManager.Logger;
+            }
+        }
+
+        public JavascriptContext(object context, JavascriptContextHandlers handlers)
+        {
+            this.Context = context;
+            this.Handlers = handlers;
+        }
+
+        public object Context { get; private set; }
+
+        public JavascriptContextHandlers Handlers { get; private set; }
+
+        public object GetParameter(string name)
+        {
+            return this.Handlers.GetParameter(this.Context, name);
+        }
+
+        public void SetParameter(string name, object value)
+        {
+            this.Handlers.SetParameter(this.Context, name, value);
+        }
+
+        public object Run(string script)
+        {
+            return this.Handlers.Run(this.Context, script);
+        }
+
+        public bool IsDisposed { get; private set; }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (this.IsDisposed || !disposing)
+            {
+                return;
+            }
+            this.OnDisposing();
+            this.IsDisposed = true;
+        }
+
+        protected virtual void OnDisposing()
+        {
+            if (this.Context is IDisposable disposable)
+            {
+                disposable.Dispose();
+            }
+        }
+
+        ~JavascriptContext()
+        {
+            Logger.Write(this.GetType(), LogLevel.Error, "Component was not disposed: {0}", this.GetType().Name);
+            try
+            {
+                this.Dispose(true);
+            }
+            catch
+            {
+                //Nothing can be done, never throw on GC thread.
+            }
+        }
+    }
+
+    public class JavascriptContextHandlers
+    {
+        public JavascriptContextHandlers(Func<object, string, object> getParameter, Action<object, string, object> setParameter, Func<object, string, object> run)
+        {
+            this.GetParameter = getParameter;
+            this.SetParameter = setParameter;
+            this.Run = run;
+        }
+
+        public Func<object, string, object> GetParameter { get; private set; }
+
+        public Action<object, string, object> SetParameter { get; private set; }
+
+        public Func<object, string, object> Run { get; private set; }
+    }
+}
---
+++ FoxTunes.Scripting.JS.Noesis/Proxies/JavascriptContextFactory.cs
@@ -0,0 +1,78 @@
+﻿using FoxDb;
+using System;
+using System.IO;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace FoxTunes.Proxies
+{
+    public static class JavascriptContextFactory
+    {
+        public static readonly Lazy<Assembly> Assembly = new Lazy<Assembly>(() =>
+        {
+            var fileName = Path.Combine(
+                Path.GetDirectoryName(typeof(JavascriptContextFactory).Assembly.Location),
+                Environment.Is64BitProcess ? "x64" : "x86",
+                "Noesis.Javascript.dll"
+            );
+            return global::System.Reflection.Assembly.LoadFrom(fileName);
+        });
+
+        public static readonly Lazy<Type> Type = new Lazy<Type>(() =>
+        {
+            return Assembly.Value.GetType("Noesis.Javascript.JavascriptContext");
+        });
+
+        public static readonly Lazy<JavascriptContextHandlers> Handlers = new Lazy<JavascriptContextHandlers>(() =>
+        {
+            return new JavascriptContextHandlers(GetParameter(), SetParameter(), Run());
+        });
+
+        private static Func<object, string, object> GetParameter()
+        {
+            var type = Type.Value;
+            var context = Expression.Parameter(typeof(object));
+            var name = Expression.Parameter(typeof(string));
+            var method = type.GetMethod("GetParameter", BindingFlags.Public | BindingFlags.Instance, null, new[] { typeof(string) }, null);
+            return Expression.Lambda<Func<object, string, object>>(
+                Expression.Call(Expression.Convert(context, type), method, name),
+                context,
+                name
+            ).Compile();
+        }
+
+        private static Action<object, string, object> SetParameter()
+        {
+            var type = Type.Value;
+            var context = Expression.Parameter(typeof(object));
+            var name = Expression.Parameter(typeof(string));
+            var value = Expression.Parameter(typeof(object));
+            var method = type.GetMethod("SetParameter", BindingFlags.Public | BindingFlags.Instance, null, new[] { typeof(string), typeof(object) }, null);
+            return Expression.Lambda<Action<object, string, object>>(
+                Expression.Call(Expression.Convert(context, type), method, name, value),
+                context,
+                name,
+                value
+            ).Compile();
+        }
+
+        private static Func<object, string, object> Run()
+        {
+            var type = Type.Value;
+            var context = Expression.Parameter(typeof(object));
+            var script = Expression.Parameter(typeof(string));
+            var method = type.GetMethod("Run", BindingFlags.Public | BindingFlags.Instance, null, new[] { typeof(string) }, null);
+            return Expression.Lambda<Func<object, string, object>>(
+                Expression.Call(Expression.Convert(context, type), method, script),
+                context,
+                script
+            ).Compile();
+        }
+
+        public static JavascriptContext Create()
+        {
+            var context = FastActivator.Instance.Activate(Type.Value);
+            return new JavascriptContext(context, Handlers.Value);
+        }
+    }
+}
