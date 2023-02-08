6197b56398975418aa1e8533d5d66239c9cb1307
aidan <aidan.gilbert@live.co.uk>
2023-02-08 10:57:04 +0000

    Move Selection Properties to new plugin.

---
+++ FoxTunes.UI.Windows.MetaDataViewer/ViewModel/StringResources.cs
@@ -0,0 +1,49 @@
+﻿namespace FoxTunes.ViewModel
+{
+    /// <summary>
+    /// This class just exposes the internal properties of <see cref="Strings"/> so we don't need to use PublicResXFileCodeGenerator.
+    /// PublicResXFileCodeGenerator causes many type import conflict warnings.
+    /// </summary>
+    public static class StringResources
+    {
+        public static string SelectionProperties_Tags
+        {
+            get
+            {
+                return Strings.SelectionProperties_Tags;
+            }
+        }
+
+        public static string SelectionProperties_Properties
+        {
+            get
+            {
+                return Strings.SelectionProperties_Properties;
+            }
+        }
+
+        public static string SelectionProperties_Location
+        {
+            get
+            {
+                return Strings.SelectionProperties_Location;
+            }
+        }
+
+        public static string SelectionProperties_Images
+        {
+            get
+            {
+                return Strings.SelectionProperties_Images;
+            }
+        }
+
+        public static string SelectionProperties_ReplayGain
+        {
+            get
+            {
+                return Strings.SelectionProperties_ReplayGain;
+            }
+        }
+    }
+}
