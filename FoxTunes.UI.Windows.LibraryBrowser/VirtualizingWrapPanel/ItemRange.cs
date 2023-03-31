9a4b5aa2e84d672280f0ec1ec4b3cc640129144a
aidan <aidan.gilbert@live.co.uk>
2023-03-31 15:03:49 +0100

    Use sbaeumlisberger/VirtualizingWrapPanel for later targets.
     See https://github.com/sbaeumlisberger/VirtualizingWrapPanel
    
     Thank you, sbaeumlisberger.

---
+++ FoxTunes.UI.Windows.LibraryBrowser/VirtualizingWrapPanel/ItemRange.cs
@@ -0,0 +1,23 @@
+﻿//Content from https://github.com/sbaeumlisberger/VirtualizingWrapPanel
+#if NET40
+#else
+namespace FoxTunes
+{
+    public struct ItemRange
+    {
+        public int StartIndex { get; }
+        public int EndIndex { get; }
+
+        public ItemRange(int startIndex, int endIndex) : this()
+        {
+            StartIndex = startIndex;
+            EndIndex = endIndex;
+        }
+
+        public bool Contains(int itemIndex)
+        {
+            return itemIndex >= StartIndex && itemIndex <= EndIndex;
+        }
+    }
+}
+#endif
