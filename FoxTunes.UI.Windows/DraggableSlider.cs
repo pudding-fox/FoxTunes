8ad6185afa1ea26558187d14979d083fe11463c3
pudding <pudding@raimusoft.com>
2024-01-28 14:04:14 +0000

    Initial volume slider styling.

---
+++ FoxTunes.UI.Windows/DraggableSlider.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace FoxTunes
+{
+    public class DraggableSlider : Slider
+    {
+        public DraggableSlider()
+        {
+            this.IsMoveToPointEnabled = true;
+        }
+
+        public Track Track { get; private set; }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            this.Track = this.Template.FindName("PART_Track", this) as Track;
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.Value = this.Track.ValueFromPoint(e.GetPosition(this.Track));
+            }
+        }
+
+        protected override void OnPreviewMouseDown(MouseButtonEventArgs e)
+        {
+            base.OnPreviewMouseDown(e);
+            ((UIElement)e.OriginalSource).CaptureMouse();
+        }
+
+        protected override void OnPreviewMouseUp(MouseButtonEventArgs e)
+        {
+            base.OnPreviewMouseUp(e);
+            ((UIElement)e.OriginalSource).ReleaseMouseCapture();
+        }
+    }
+}
