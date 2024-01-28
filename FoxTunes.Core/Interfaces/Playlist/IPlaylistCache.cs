3df18c7a1997cf9cb75ca6f562f428fdf5c52c3e
pudding <pudding@raimusoft.com>
2024-01-28 14:04:09 +0000

    Fixes some unnecessary UI refreshes when updating meta data.

--- FoxTunes.Core/Interfaces/Playlist/IPlaylistCache.cs
+++ FoxTunes.Core/Interfaces/Playlist/IPlaylistCache.cs
@@ -5,6 +5,8 @@ namespace FoxTunes.Interfaces
 {
     public interface IPlaylistCache : IStandardComponent
     {
+        bool Contains(Func<PlaylistItem, bool> predicate);
+
         IEnumerable<PlaylistItem> GetItems(Func<IEnumerable<PlaylistItem>> factory);
     }
 }
