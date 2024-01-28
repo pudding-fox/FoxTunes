cae1b17f7638db0b710dc0f1d8b08a74321b1d28
pudding <pudding@raimusoft.com>
2024-01-28 14:04:50 +0000

    Drag/drop playlist items to another playlist.

--- FoxTunes.Core/Tasks/MovePlaylistItemsTask.cs
+++ FoxTunes.Core/Tasks/MovePlaylistItemsTask.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using FoxDb.Interfaces;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace FoxTunes
@@ -13,9 +14,31 @@ namespace FoxTunes
 
         public IEnumerable<PlaylistItem> PlaylistItems { get; private set; }
 
-        protected override Task OnRun()
+        protected override async Task OnRun()
         {
-            return this.MoveItems(this.PlaylistItems);
+            var moveItems = new List<PlaylistItem>();
+            var copyItems = new List<PlaylistItem>();
+            foreach (var playlistItem in this.PlaylistItems)
+            {
+                if (playlistItem.Playlist_Id == this.Playlist.Id)
+                {
+                    moveItems.Add(playlistItem);
+                }
+                else
+                {
+                    copyItems.Add(playlistItem);
+                }
+            }
+            if (moveItems.Count > 0)
+            {
+                await this.MoveItems(moveItems).ConfigureAwait(false);
+            }
+            if (copyItems.Count > 0)
+            {
+                await this.AddPlaylistItems(copyItems).ConfigureAwait(false);
+                await this.ShiftItems(QueryOperator.GreaterOrEqual, this.Sequence, this.Offset).ConfigureAwait(false);
+                await this.SetPlaylistItemsStatus(PlaylistItemStatus.None).ConfigureAwait(false);
+            }
         }
 
         protected override async Task OnCompleted()
