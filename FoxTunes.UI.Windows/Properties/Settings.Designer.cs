4480b63554ea0ae0fda7518f5b04ec4d467842da
pudding <pudding@raimusoft.com>
2024-01-28 14:03:20 +0000

    Makes library/playlist grid splitter adjustable.
     The position is persisted.

--- FoxTunes.UI.Windows/Properties/Settings.Designer.cs
+++ FoxTunes.UI.Windows/Properties/Settings.Designer.cs
@@ -37,7 +37,7 @@ namespace FoxTunes.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute(".35*")]
+        [global::System.Configuration.DefaultSettingValueAttribute("0.35*")]
         public string ColumnWidth_FC87DEC38708 {
             get {
                 return ((string)(this["ColumnWidth_FC87DEC38708"]));
---
+++ FoxTunes.UI.Windows/ViewModel/Converters/GridLengthConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace FoxTunes.ViewModel
+{
+    public class GridLengthConverter : System.Windows.GridLengthConverter, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return this.ConvertFrom(value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is GridLength)
+            {
+                var length = (GridLength)value;
+                if (length.IsStar)
+                {
+                    value = new GridLength(length.Value, GridUnitType.Pixel);
+                }
+            }
+            return this.ConvertTo(value, targetType);
+        }
+    }
+}
