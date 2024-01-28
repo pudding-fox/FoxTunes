f0de11ccc834d664c8ceaaaf32db6c3a0c4e09a5
pudding <pudding@raimusoft.com>
2024-01-28 14:03:11 +0000

    Making Menu configurable/reusable

---
+++ FoxTunes.UI.Windows/Menu.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FoxTunes
+{
+    /// <summary>
+    /// Interaction logic for Menu.xaml
+    /// </summary>
+    public partial class Menu : ContextMenu
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty CategoryProperty = DependencyProperty.Register(
+            "Category",
+            typeof(string),
+            typeof(Menu)
+        );
+
+        public static string GetCategory(Menu source)
+        {
+            return (string)source.GetValue(CategoryProperty);
+        }
+
+        public static void SetCategory(Menu source, string value)
+        {
+            source.SetValue(CategoryProperty, value);
+        }
+
+        public string Category
+        {
+            get
+            {
+                return this.GetValue(CategoryProperty) as string;
+            }
+            set
+            {
+                this.SetValue(CategoryProperty, value);
+            }
+        }
+    }
+}
