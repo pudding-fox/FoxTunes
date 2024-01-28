9bd9c774ae0c6b4190d393ab1ae6a909a60f920a
pudding <pudding@raimusoft.com>
2024-01-28 14:02:58 +0000

    Various fixes.
     Trim meta data and property text values.
     Deletes all but expression dark theme. They are too expensive to
     maintain right now.
     Avoid re-generating library hierarchy where possible.
     Remember selected/expanded nodes when changing hierarchy view.

--- FoxTunes.UI.Windows/ViewModel/RenderableLibraryHierarchyItem.cs
+++ FoxTunes.UI.Windows/ViewModel/RenderableLibraryHierarchyItem.cs
@@ -1,4 +1,5 @@
 ﻿using FoxTunes.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -15,47 +16,112 @@ namespace FoxTunes.ViewModel
         public RenderableLibraryHierarchyItem(LibraryHierarchyItem libraryHierarchyItem, IDatabase database) : this()
         {
             this.LibraryHierarchyItem = libraryHierarchyItem;
+            this.Database = database;
             this.Id = libraryHierarchyItem.Id;
             this.DisplayValue = libraryHierarchyItem.DisplayValue;
             this.SortValue = libraryHierarchyItem.SortValue;
             this.IsLeaf = libraryHierarchyItem.IsLeaf;
-            this.Database = database;
+
         }
 
         public LibraryHierarchyItem LibraryHierarchyItem { get; private set; }
+
+        public IDatabase Database { get; private set; }
+
+        private ObservableCollection<LibraryHierarchyItem> _Children { get; set; }
 
         public override ObservableCollection<LibraryHierarchyItem> Children
         {
             get
             {
-                var sequence = default(IEnumerable<LibraryHierarchyItem>);
-                if (this.IsLeaf)
+                if (this._Children == null)
                 {
-                    sequence = Enumerable.Empty<LibraryHierarchyItem>();
+                    var sequence = default(IEnumerable<LibraryHierarchyItem>);
+                    if (this.IsLeaf)
+                    {
+                        sequence = Enumerable.Empty<LibraryHierarchyItem>();
+                    }
+                    else
+                    {
+                        var query = this.Database
+                            .GetMemberQuery<LibraryHierarchyItem, LibraryHierarchyItem>(this.LibraryHierarchyItem, _ => _.Children)
+                            .ToArray(); //We have to switch to object query as the following projection is not supported.
+                        sequence = query.Select(libraryHierarchyItem => new RenderableLibraryHierarchyItem(libraryHierarchyItem, this.Database));
+                    }
+                    this._Children = new ObservableCollection<LibraryHierarchyItem>(sequence);
                 }
-                else
-                {
-                    var query = this.Database
-                        .GetMemberQuery<LibraryHierarchyItem, LibraryHierarchyItem>(this.LibraryHierarchyItem, _ => _.Children)
-                        .ToArray(); //We have to switch to object query as the following projection is not supported.
-                    sequence = query.Select(libraryHierarchyItem => new RenderableLibraryHierarchyItem(libraryHierarchyItem, this.Database));
-                }
-                return new ObservableCollection<LibraryHierarchyItem>(sequence);
+                return this._Children;
             }
         }
+
+        private ObservableCollection<LibraryItem> _Items { get; set; }
 
         public override ObservableCollection<LibraryItem> Items
         {
             get
             {
-                var query = this.Database.GetMemberQuery<LibraryHierarchyItem, LibraryItem>(this.LibraryHierarchyItem, _ => _.Items);
-                query.Include("MetaDatas");
-                query.Include("Properties");
-                query.Include("Statistics");
-                return new ObservableCollection<LibraryItem>(query);
+                if (this._Items == null)
+                {
+                    var query = this.Database.GetMemberQuery<LibraryHierarchyItem, LibraryItem>(this.LibraryHierarchyItem, _ => _.Items);
+                    query.Include("MetaDatas");
+                    query.Include("Properties");
+                    query.Include("Statistics");
+                    this._Items = new ObservableCollection<LibraryItem>(query);
+                }
+                return this._Items;
             }
         }
 
-        public IDatabase Database { get; private set; }
+        private bool _IsSelected { get; set; }
+
+        public bool IsSelected
+        {
+            get
+            {
+                return this._IsSelected;
+            }
+            set
+            {
+                this._IsSelected = value;
+                this.OnIsSelectedChanged();
+            }
+        }
+
+        protected virtual void OnIsSelectedChanged()
+        {
+            if (this.IsSelectedChanged != null)
+            {
+                this.IsSelectedChanged(this, EventArgs.Empty);
+            }
+            this.OnPropertyChanged("IsSelected");
+        }
+
+        public event EventHandler IsSelectedChanged = delegate { };
+
+        private bool _IsExpanded { get; set; }
+
+        public bool IsExpanded
+        {
+            get
+            {
+                return this._IsExpanded;
+            }
+            set
+            {
+                this._IsExpanded = value;
+                this.OnIsExpandedChanged();
+            }
+        }
+
+        protected virtual void OnIsExpandedChanged()
+        {
+            if (this.IsExpandedChanged != null)
+            {
+                this.IsExpandedChanged(this, EventArgs.Empty);
+            }
+            this.OnPropertyChanged("IsExpanded");
+        }
+
+        public event EventHandler IsExpandedChanged = delegate { };
     }
 }
