4fa262132c5e9664c847e89d1c81e91a949e431f
pudding <pudding@raimusoft.com>
2024-01-28 14:04:28 +0000

    Removing visibility tracking image loading logic.
     It just doesn't seem to work very well. Just loading the image directly
     from LibraryBrowserTile.Loaded seems to work better.
    
     We really need to virtualize the list.

--- FoxTunes.UI.Windows/Utilities/ImageLoaderConfiguration.cs
+++ FoxTunes.UI.Windows/Utilities/ImageLoaderConfiguration.cs
@@ -12,10 +12,6 @@ namespace FoxTunes
 
         public const string CACHE_SIZE = "BBBBDADA-446D-490B-BDDC-446CCC063AD5";
 
-        public const string THREADS = "CCCC8536-6631-4B67-A84A-00AF6218E7BA";
-
-        public const string INTERVAL = "DDDD3458-2514-478A-BC80-8731CAE69C88";
-
         public static IEnumerable<ConfigurationSection> GetConfigurationSections()
         {
             var cacheSize = default(int);
@@ -31,9 +27,7 @@ namespace FoxTunes
             }
             yield return new ConfigurationSection(SECTION, "Images")
                 .WithElement(new BooleanConfigurationElement(HIGH_QUALITY_RESIZER, "High Quality Resizer").WithValue(true))
-                .WithElement(new IntegerConfigurationElement(CACHE_SIZE, "Cache Size", path: "Advanced").WithValue(cacheSize).WithValidationRule(new IntegerValidationRule(64, 2048)))
-                .WithElement(new IntegerConfigurationElement(THREADS, "Background Threads", path: "Advanced").WithValue(Environment.ProcessorCount).WithValidationRule(new IntegerValidationRule(1, 32)))
-                .WithElement(new IntegerConfigurationElement(INTERVAL, "Interval", path: "Advanced").WithValue(10).WithValidationRule(new IntegerValidationRule(0, 100))
+                .WithElement(new IntegerConfigurationElement(CACHE_SIZE, "Cache Size", path: "Advanced").WithValue(cacheSize).WithValidationRule(new IntegerValidationRule(64, 2048))
             );
         }
     }
