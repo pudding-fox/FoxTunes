a0b30aa13200d12c0d9377bb7a2be45dd0a8cb6d
pudding <pudding@raimusoft.com>
2024-01-28 14:05:05 +0000

    Stubbing out some non client interaction handlers.

--- FoxTunes.UI.Windows.Snapping/Utilities/SnappingWindow.cs
+++ FoxTunes.UI.Windows.Snapping/Utilities/SnappingWindow.cs
@@ -3,11 +3,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
+using System.Windows.Interop;
 
 namespace FoxTunes
 {
     public class SnappingWindow : BaseComponent, IDisposable
     {
+        public const int WM_NCLBUTTONDOWN = 0x00A1;
+
+        public const int NC_CAPTION = 2;
+
+        public const int NC_LEFT = 10;
+
+        public const int NC_RIGHT = 11;
+
+        public const int NC_TOP = 12;
+
+        public const int NC_TOPLEFT = 13;
+
+        public const int NC_TOPRIGHT = 14;
+
+        public const int NC_BOTTOM = 15;
+
+        public const int NC_BOTTOMLEFT = 16;
+
+        public const int NC_BOTTOMRIGHT = 17;
+
         static SnappingWindow()
         {
             Instances = new List<WeakReference<SnappingWindow>>();
@@ -40,6 +61,16 @@ namespace FoxTunes
 
         public static event EventHandler ActiveChanged;
 
+        public static int X;
+
+        public static int Y;
+
+        public static bool IsMoving;
+
+        public static bool IsResizing;
+
+        public static ResizeDirection ResizeDirection;
+
         private SnappingWindow()
         {
             lock (Instances)
@@ -53,15 +84,93 @@ namespace FoxTunes
         {
             this.Handle = handle;
             this.Window = GetWindow(handle);
+            this.Callback = new HwndSourceHook(this.OnCallback);
         }
 
         public IntPtr Handle { get; private set; }
 
         public Window Window { get; private set; }
 
+        public HwndSourceHook Callback { get; private set; }
+
         public override void InitializeComponent(ICore core)
         {
+            this.AddHook();
             base.InitializeComponent(core);
+        }
+
+        protected virtual void AddHook()
+        {
+            Logger.Write(this, LogLevel.Debug, "Adding Windows event handler.");
+            var source = HwndSource.FromHwnd(this.Handle);
+            if (source == null)
+            {
+                Logger.Write(this, LogLevel.Warn, "No such window for handle: {0}", handle);
+                return;
+            }
+            source.AddHook(this.Callback);
+        }
+
+        protected virtual IntPtr OnCallback(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == WM_NCLBUTTONDOWN)
+            {
+                var area = wParam.ToInt32();
+                var low = (lParam.ToInt32() & 0xFFFF);
+                var high = (lParam.ToInt32() & 0xFFFF) >> 16;
+                X = Convert.ToInt32(low);
+                Y = Convert.ToInt32(high);
+                if (this.OnNonClientButtonDown(area))
+                {
+                    return new IntPtr(1);
+                }
+            }
+            return IntPtr.Zero;
+        }
+
+        protected virtual bool OnNonClientButtonDown(int area)
+        {
+            switch (area)
+            {
+                case NC_CAPTION:
+                    this.OnMove();
+                    return true;
+                case NC_TOPLEFT:
+                    this.OnResize(ResizeDirection.Top | ResizeDirection.Left);
+                    return true;
+                case NC_TOP:
+                    this.OnResize(ResizeDirection.Top);
+                    return true;
+                case NC_TOPRIGHT:
+                    this.OnResize(ResizeDirection.Top | ResizeDirection.Right);
+                    return true;
+                case NC_RIGHT:
+                    this.OnResize(ResizeDirection.Right);
+                    return true;
+                case NC_BOTTOMRIGHT:
+                    this.OnResize(ResizeDirection.Bottom | ResizeDirection.Right);
+                    return true;
+                case NC_BOTTOM:
+                    this.OnResize(ResizeDirection.Bottom);
+                    return true;
+                case NC_BOTTOMLEFT:
+                    this.OnResize(ResizeDirection.Bottom | ResizeDirection.Left);
+                    return true;
+                case NC_LEFT:
+                    this.OnResize(ResizeDirection.Left);
+                    return true;
+            }
+            return false;
+        }
+
+        protected virtual void OnMove()
+        {
+
+        }
+
+        protected virtual void OnResize(ResizeDirection direction)
+        {
+
         }
 
         public bool IsDisposed { get; private set; }
@@ -129,5 +238,15 @@ namespace FoxTunes
             }
             throw new InvalidOperationException(string.Format("Window for handle \"{0}\" could not be found.", handle));
         }
+    }
+
+    [Flags]
+    public enum ResizeDirection : byte
+    {
+        None = 0,
+        Left = 1,
+        Right = 2,
+        Top = 4,
+        Bottom = 8
     }
 }
