a2ac89178868a70167d2474c0099bc33a678172c
pudding <pudding@raimusoft.com>
2024-01-28 14:05:48 +0000

    Adds new theme: Adamantine

--- FoxTunes.UI.Windows.Themes/Properties/Strings.Designer.cs
+++ FoxTunes.UI.Windows.Themes/Properties/Strings.Designer.cs
@@ -19,7 +19,7 @@ namespace FoxTunes {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Strings {
@@ -57,6 +57,33 @@ namespace FoxTunes {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to .
+        /// </summary>
+        internal static string AdamantineTheme_Description {
+            get {
+                return ResourceManager.GetString("AdamantineTheme.Description", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Adamantine.
+        /// </summary>
+        internal static string AdamantineTheme_Name {
+            get {
+                return ResourceManager.GetString("AdamantineTheme.Name", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to List Row Shading.
+        /// </summary>
+        internal static string AdamantineThemeConfiguration_ListRowShading {
+            get {
+                return ResourceManager.GetString("AdamantineThemeConfiguration.ListRowShading", resourceCulture);
             }
         }
         
