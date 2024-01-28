391154339841018279de051bb3161beddfca628a
pudding <pudding@raimusoft.com>
2024-01-28 14:04:14 +0000

    Adds volume slider to main window.
     It needs styling.

---
+++ FoxTunes.UI.Windows/ViewModel/Volume.cs
@@ -0,0 +1,68 @@
+﻿using FoxTunes.Interfaces;
+using System;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FoxTunes.ViewModel
+{
+    public class Volume : ViewModelBase
+    {
+        public float Value
+        {
+            get
+            {
+                if (this.Output == null)
+                {
+                    return 0;
+                }
+                return this.Output.Volume;
+            }
+            set
+            {
+                if (this.Output == null)
+                {
+                    return;
+                }
+                this.Output.Volume = value;
+                this.OnValueChanged();
+            }
+        }
+
+        protected virtual void OnValueChanged()
+        {
+            if (this.ValueChanged != null)
+            {
+                this.ValueChanged(this, EventArgs.Empty);
+            }
+            this.OnPropertyChanged("Value");
+        }
+
+        public event EventHandler ValueChanged;
+
+        public IOutput Output { get; private set; }
+
+        protected override void OnCoreChanged()
+        {
+            this.Output = this.Core.Components.Output;
+            this.Output.VolumeChanged += this.OnVolumeChanged;
+            //TODO: Bad .Wait().
+            this.Refresh().Wait();
+            base.OnCoreChanged();
+        }
+
+        protected virtual void OnVolumeChanged(object sender, EventArgs e)
+        {
+            var task = this.Refresh();
+        }
+
+        protected virtual Task Refresh()
+        {
+            return Windows.Invoke(new Action(this.OnValueChanged));
+        }
+
+        protected override Freezable CreateInstanceCore()
+        {
+            return new Volume();
+        }
+    }
+}
---
+++ FoxTunes.UI.Windows/Volume.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace FoxTunes
+{
+    /// <summary>
+    /// Interaction logic for Volume.xaml
+    /// </summary>
+    public partial class Volume : UserControl
+    {
+        public Volume()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
