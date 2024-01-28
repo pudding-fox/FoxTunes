2b8bc0df3e1077a42bb84c2513bbac3f07c81c2d
pudding <pudding@raimusoft.com>
2024-01-28 14:04:21 +0000

    Adds "on demand" replay gain scanner option.
     This will calculate replay gain before a track is played if no meta
     data is available (the cauclated track values will be saved).

--- FoxTunes.Output.Bass.ReplayGain/BassReplayGainBehaviour.cs
+++ FoxTunes.Output.Bass.ReplayGain/BassReplayGainBehaviour.cs
@@ -1,10 +1,14 @@
 ﻿using FoxTunes.Interfaces;
+using ManagedBass.ReplayGain;
 using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace FoxTunes
 {
+    [ComponentDependency(Slot = ComponentSlots.Output)]
     public class BassReplayGainBehaviour : StandardBehaviour, IConfigurableComponent, IDisposable
     {
         public BassReplayGainBehaviour()
@@ -20,15 +24,25 @@ namespace FoxTunes
 
         public IBassStreamPipelineFactory BassStreamPipelineFactory { get; private set; }
 
+        public IMetaDataManager MetaDataManager { get; private set; }
+
+        public ISignalEmitter SignalEmitter { get; private set; }
+
         public IConfiguration Configuration { get; private set; }
 
         public ReplayGainMode Mode { get; private set; }
+
+        public bool OnDemand { get; private set; }
+
+        public bool WriteTags { get; private set; }
 
         public override void InitializeComponent(ICore core)
         {
             this.Core = core;
             this.Output = ComponentRegistry.Instance.GetComponent<IBassOutput>();
             this.BassStreamPipelineFactory = ComponentRegistry.Instance.GetComponent<IBassStreamPipelineFactory>();
+            this.MetaDataManager = core.Managers.MetaData;
+            this.SignalEmitter = core.Components.SignalEmitter;
             this.Configuration = core.Components.Configuration;
             this.Configuration.GetElement<BooleanConfigurationElement>(
                 BassOutputConfiguration.SECTION,
@@ -48,11 +62,20 @@ namespace FoxTunes
                 BassOutputConfiguration.SECTION,
                 BassReplayGainBehaviourConfiguration.MODE
             ).ConnectValue(option => this.Mode = BassReplayGainBehaviourConfiguration.GetMode(option));
+            this.Configuration.GetElement<BooleanConfigurationElement>(
+                BassOutputConfiguration.SECTION,
+                BassReplayGainBehaviourConfiguration.ON_DEMAND
+            ).ConnectValue(value => this.OnDemand = value);
+            this.Configuration.GetElement<BooleanConfigurationElement>(
+                BassOutputConfiguration.SECTION,
+                BassReplayGainScannerBehaviourConfiguration.WRITE_TAGS
+            ).ConnectValue(value => this.WriteTags = value);
             base.InitializeComponent(core);
         }
 
         public void Enable()
         {
+            BassReplayGain.Init();
             if (this.Output != null)
             {
                 this.Output.Loaded += this.OnLoaded;
@@ -75,6 +98,7 @@ namespace FoxTunes
             {
                 this.BassStreamPipelineFactory.CreatingPipeline -= this.OnCreatingPipeline;
             }
+            BassReplayGain.Free();
         }
 
         protected virtual void OnLoaded(object sender, OutputStreamEventArgs e)
@@ -110,13 +134,29 @@ namespace FoxTunes
             {
                 return;
             }
-            var replayGain = default(float);
+            var gain = default(float);
+            var peak = default(float);
             var mode = default(ReplayGainMode);
-            if (!this.TryGetReplayGain(stream.PlaylistItem, out replayGain, out mode))
+            if (!this.TryGetReplayGain(stream, out gain, out mode))
             {
-                return;
+                if (this.OnDemand)
+                {
+                    if (!this.TryCalculateReplayGain(stream, out gain, out peak, out mode))
+                    {
+                        return;
+                    }
+#if NET40
+                    var task = TaskEx.Run(() => this.UpdateMetaData(stream, gain, peak, mode));
+#else
+                    var task = Task.Run(() => this.UpdateMetaData(stream, gain, peak, mode));
+#endif
+                }
+                else
+                {
+                    return;
+                }
             }
-            var effect = new ReplayGainEffect(stream.ChannelHandle, replayGain, mode);
+            var effect = new ReplayGainEffect(stream.ChannelHandle, gain, mode);
             effect.Activate();
             this.Effects.Add(stream, effect);
         }
@@ -131,13 +171,13 @@ namespace FoxTunes
             }
         }
 
-        protected virtual bool TryGetReplayGain(PlaylistItem playlistItem, out float replayGain, out ReplayGainMode mode)
+        protected virtual bool TryGetReplayGain(BassOutputStream stream, out float replayGain, out ReplayGainMode mode)
         {
             var albumGain = default(float);
             var trackGain = default(float);
-            lock (playlistItem.MetaDatas)
+            lock (stream.PlaylistItem.MetaDatas)
             {
-                foreach (var metaDataItem in playlistItem.MetaDatas)
+                foreach (var metaDataItem in stream.PlaylistItem.MetaDatas)
                 {
                     if (string.Equals(metaDataItem.Name, CommonMetaData.ReplayGainAlbumGain, StringComparison.OrdinalIgnoreCase))
                     {
@@ -150,7 +190,7 @@ namespace FoxTunes
                             }
                             if (this.Mode == ReplayGainMode.Album)
                             {
-                                Logger.Write(this, LogLevel.Debug, "Found preferred replay gain data for album:  \"{0}\" => {1}", playlistItem.FileName, albumGain);
+                                Logger.Write(this, LogLevel.Debug, "Found preferred replay gain data for album:  \"{0}\" => {1}", stream.FileName, albumGain);
                                 mode = ReplayGainMode.Album;
                                 replayGain = albumGain;
                                 return true;
@@ -168,7 +208,7 @@ namespace FoxTunes
                             }
                             if (this.Mode == ReplayGainMode.Track)
                             {
-                                Logger.Write(this, LogLevel.Debug, "Found preferred replay gain data for track:  \"{0}\" => {1}", playlistItem.FileName, trackGain);
+                                Logger.Write(this, LogLevel.Debug, "Found preferred replay gain data for track:  \"{0}\" => {1}", stream.FileName, trackGain);
                                 mode = ReplayGainMode.Track;
                                 replayGain = trackGain;
                                 return true;
@@ -179,19 +219,19 @@ namespace FoxTunes
             }
             if (this.IsValidReplayGain(albumGain))
             {
-                Logger.Write(this, LogLevel.Debug, "Using album replay gain data: \"{0}\" => {1}", playlistItem.FileName, albumGain);
+                Logger.Write(this, LogLevel.Debug, "Using album replay gain data: \"{0}\" => {1}", stream.FileName, albumGain);
                 mode = ReplayGainMode.Album;
                 replayGain = albumGain;
                 return true;
             }
             if (this.IsValidReplayGain(trackGain))
             {
-                Logger.Write(this, LogLevel.Debug, "Using track replay gain data: \"{0}\" => {1}", playlistItem.FileName, trackGain);
+                Logger.Write(this, LogLevel.Debug, "Using track replay gain data: \"{0}\" => {1}", stream.FileName, trackGain);
                 mode = ReplayGainMode.Track;
                 replayGain = trackGain;
                 return true;
             }
-            Logger.Write(this, LogLevel.Debug, "No replay gain data: \"{0}\".", playlistItem.FileName);
+            Logger.Write(this, LogLevel.Debug, "No replay gain data: \"{0}\".", stream.FileName);
             mode = ReplayGainMode.None;
             replayGain = 0;
             return false;
@@ -201,6 +241,91 @@ namespace FoxTunes
         {
             //TODO: I'm sure there is a valid range of values.
             return replayGain != 0 && !float.IsNaN(replayGain);
+        }
+
+        protected virtual bool TryCalculateReplayGain(BassOutputStream stream, out float gain, out float peak, out ReplayGainMode mode)
+        {
+            Logger.Write(this, LogLevel.Debug, "Attempting to calculate track replay gain for file \"{0}\".", stream.FileName);
+            try
+            {
+                var info = default(ReplayGainInfo);
+                if (!BassReplayGain.Process(stream.ChannelHandle, out info))
+                {
+                    Logger.Write(this, LogLevel.Warn, "Failed to calculate track replay gain for file \"{0}\".", stream.FileName);
+                    gain = 0;
+                    peak = 0;
+                    mode = ReplayGainMode.None;
+                    return false;
+                }
+                Logger.Write(this, LogLevel.Debug, "Calculated track replay gain for file \"{0}\": {1}dB", stream.FileName, ReplayGainEffect.GetVolume(info.gain));
+                gain = info.gain;
+                peak = info.peak;
+                mode = ReplayGainMode.Track;
+                return true;
+            }
+            finally
+            {
+                stream.Position = 0;
+            }
+        }
+
+        protected virtual async Task UpdateMetaData(BassOutputStream stream, float gain, float peak, ReplayGainMode mode)
+        {
+            var names = new HashSet<string>();
+            lock (stream.PlaylistItem.MetaDatas)
+            {
+                var metaDatas = stream.PlaylistItem.MetaDatas.ToDictionary(
+                    element => element.Name,
+                    StringComparer.OrdinalIgnoreCase
+                );
+                var metaDataItem = default(MetaDataItem);
+                if (gain != 0)
+                {
+                    var name = default(string);
+                    switch (mode)
+                    {
+                        case ReplayGainMode.Album:
+                            name = CommonMetaData.ReplayGainAlbumGain;
+                            break;
+                        case ReplayGainMode.Track:
+                            name = CommonMetaData.ReplayGainTrackGain;
+                            break;
+                    }
+                    if (!metaDatas.TryGetValue(name, out metaDataItem))
+                    {
+                        metaDataItem = new MetaDataItem(name, MetaDataItemType.Tag);
+                        stream.PlaylistItem.MetaDatas.Add(metaDataItem);
+                    }
+                    metaDataItem.Value = Convert.ToString(gain);
+                    names.Add(name);
+                }
+                if (peak != 0)
+                {
+                    var name = default(string);
+                    switch (mode)
+                    {
+                        case ReplayGainMode.Album:
+                            name = CommonMetaData.ReplayGainAlbumPeak;
+                            break;
+                        case ReplayGainMode.Track:
+                            name = CommonMetaData.ReplayGainTrackPeak;
+                            break;
+                    }
+                    if (!metaDatas.TryGetValue(name, out metaDataItem))
+                    {
+                        metaDataItem = new MetaDataItem(name, MetaDataItemType.Tag);
+                        stream.PlaylistItem.MetaDatas.Add(metaDataItem);
+                    }
+                    metaDataItem.Value = Convert.ToString(peak);
+                    names.Add(name);
+                }
+            }
+            await this.MetaDataManager.Save(
+                new[] { stream.PlaylistItem },
+                this.WriteTags,
+                false,
+                names.ToArray()
+            ).ConfigureAwait(false);
         }
 
         public IEnumerable<ConfigurationSection> GetConfigurationSections()
