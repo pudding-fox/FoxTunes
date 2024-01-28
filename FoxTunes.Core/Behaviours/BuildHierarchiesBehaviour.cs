a36ec9a82f3383e4fcd101cc6daa5fa9b33356d0
pudding <pudding@raimusoft.com>
2024-01-28 14:03:01 +0000

    Some refactoring.
     Moving hierarchy concerns to HierarchyManager.
     Start using signals to trigger some updates.

---
+++ FoxTunes.Core/Behaviours/BuildHierarchiesBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using FoxTunes.Interfaces;
+
+namespace FoxTunes.Behaviours
+{
+    public class BuildHierarchiesBehaviour : StandardBehaviour
+    {
+        public ICore Core { get; private set; }
+
+        public IHierarchyManager HierarchyManager { get; private set; }
+
+        public ISignalEmitter SignalEmitter { get; private set; }
+
+        public override void InitializeComponent(ICore core)
+        {
+            this.Core = core;
+            this.HierarchyManager = core.Managers.Hierarchy;
+            this.SignalEmitter = core.Components.SignalEmitter;
+            this.SignalEmitter.Signal += this.OnSignal;
+            base.InitializeComponent(core);
+        }
+
+        private void OnSignal(object sender, ISignal signal)
+        {
+            switch (signal.Name)
+            {
+                case CommonSignals.LibraryUpdated:
+                    this.HierarchyManager.BuildHierarchies();
+                    break;
+            }
+        }
+    }
+}
---
+++ FoxTunes.Core/Interfaces/Managers/IHierarchyManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace FoxTunes.Interfaces
+{
+    public interface IHierarchyManager : IStandardManager, IBackgroundTaskSource
+    {
+        Task AddHierarchy(LibraryHierarchy libraryHierarchy);
+
+        Task DeleteHierarchy(LibraryHierarchy libraryHierarchy);
+
+        Task BuildHierarchies();
+    }
+}
