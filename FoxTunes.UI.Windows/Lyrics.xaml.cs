bbda4ef1f0a75022060f478f40960ef72bf3b844
pudding <pudding@raimusoft.com>
2024-01-28 14:04:02 +0000

    Enable Lyrics UIComponent only when meta data is configured to read them.

--- FoxTunes.UI.Windows/Lyrics.xaml.cs
+++ FoxTunes.UI.Windows/Lyrics.xaml.cs
@@ -4,7 +4,7 @@
     /// Interaction logic for Lyrics.xaml
     /// </summary>
     [UIComponent("F7774E81-26FC-4F0C-8E0A-67214D155547", UIComponentSlots.TOP_RIGHT, "Lyrics")]
-    [UIComponentDependency(MetaDataBehaviourConfiguration.SECTION, MetaDataBehaviourConfiguration.ENABLE_ELEMENT)]
+    [UIComponentDependency(MetaDataBehaviourConfiguration.SECTION, MetaDataBehaviourConfiguration.READ_LYRICS_TAGS)]
     public partial class Lyrics : UIComponentBase
     {
         public Lyrics()
