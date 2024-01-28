95ea8a75ac30cdbbb05c221b3fe778f6d9f79d1f
pudding <pudding@raimusoft.com>
2024-01-28 14:04:29 +0000

    Hide "Windows Media Tags" when meta data is disabled.

--- FoxTunes.MetaData.TagLib/TagLibFileFactoryConfiguration.cs
+++ FoxTunes.MetaData.TagLib/TagLibFileFactoryConfiguration.cs
@@ -11,6 +11,19 @@ namespace FoxTunes
             yield return new ConfigurationSection(MetaDataBehaviourConfiguration.SECTION, "Meta Data")
                 .WithElement(new BooleanConfigurationElement(READ_WINDOWS_MEDIA_TAGS, "Windows Media Tags").WithValue(false)
             );
+            StandardComponents.Instance.Configuration.GetElement<BooleanConfigurationElement>(MetaDataBehaviourConfiguration.SECTION, MetaDataBehaviourConfiguration.ENABLE_ELEMENT).ConnectValue(value => UpdateConfiguration(value));
+        }
+
+        private static void UpdateConfiguration(bool enabled)
+        {
+            if (enabled)
+            {
+                StandardComponents.Instance.Configuration.GetElement(MetaDataBehaviourConfiguration.SECTION, READ_WINDOWS_MEDIA_TAGS).Show();
+            }
+            else
+            {
+                StandardComponents.Instance.Configuration.GetElement(MetaDataBehaviourConfiguration.SECTION, READ_WINDOWS_MEDIA_TAGS).Hide();
+            }
         }
     }
 }
