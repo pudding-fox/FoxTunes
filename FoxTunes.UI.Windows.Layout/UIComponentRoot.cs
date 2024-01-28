36917578c0d63868664f85cad7b82fcdb327e98c
pudding <pudding@raimusoft.com>
2024-01-28 14:04:55 +0000

    Connect the IsInDesignMode property to the designer overlay.

---
+++ FoxTunes.UI.Windows.Layout/UIComponentRoot.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Data;
+
+namespace FoxTunes
+{
+    public class UIComponentRoot : UIComponentPanel
+    {
+        public UIComponentRoot()
+        {
+            var container = new UIComponentContainer();
+            container.SetBinding(
+                UIComponentContainer.ComponentProperty,
+                new Binding()
+                {
+                    Source = this,
+                    Path = new PropertyPath(nameof(this.Component))
+                }
+            );
+            this.Content = container;
+        }
+
+        protected override void CreateBindings()
+        {
+            //Nothing to do.
+        }
+    }
+}
