e2ba571cd272a9a7cff97d53dd82922515dc4ec8
pudding <pudding@raimusoft.com>
2024-01-28 14:03:35 +0000

    Artwork scales when manually resized with GridSplitter.

--- FoxTunes.UI.Windows/Artwork.xaml.cs
+++ FoxTunes.UI.Windows/Artwork.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using FoxTunes.Interfaces;
+using System;
 using System.IO;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Media;
 
 namespace FoxTunes
@@ -9,7 +13,7 @@ namespace FoxTunes
     /// Interaction logic for Artwork.xaml
     /// </summary>
     [UIComponent("66C8A9E7-0891-48DD-8086-E40F72D4D030", UIComponentSlots.NONE, "Artwork")]
-    public partial class Artwork : Square
+    public partial class Artwork : UIComponentBase
     {
         public static readonly IArtworkProvider ArtworkProvider = ComponentRegistry.Instance.GetComponent<IArtworkProvider>();
 
@@ -78,6 +82,67 @@ namespace FoxTunes
                     this.IsComponentEnabled = true;
                 });
             }
+        }
+
+        protected override void OnVisualParentChanged(DependencyObject oldParent)
+        {
+            if (this.Parent != null)
+            {
+                var row = default(RowDefinition);
+                var grid = this.Parent.FindAncestor<Grid>();
+                if (grid != null)
+                {
+                    var index = Grid.GetRow(this);
+                    if (index < grid.RowDefinitions.Count)
+                    {
+                        row = grid.RowDefinitions[index];
+                        BindingHelper.AddHandler(row, RowDefinition.HeightProperty, typeof(RowDefinition), (sender, e) =>
+                        {
+                            this.UpdateLayoutSource(row);
+                        });
+                    }
+                }
+                this.UpdateLayoutSource(row);
+            }
+            base.OnVisualParentChanged(oldParent);
+        }
+
+        protected virtual void UpdateLayoutSource(RowDefinition row = null)
+        {
+            if (row == null || row.Height.IsAuto || row.Height.IsStar)
+            {
+                this.SizeChanged += this.OnSizeChanged;
+            }
+            else
+            {
+                BindingOperations.ClearBinding(this, WidthProperty);
+                BindingOperations.ClearBinding(this, HeightProperty);
+            }
+        }
+
+        protected virtual void OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            BindingOperations.ClearBinding(this, WidthProperty);
+            BindingOperations.ClearBinding(this, HeightProperty);
+            if (this.ActualWidth > 0)
+            {
+                BindingOperations.SetBinding(this, HeightProperty, new Binding("ActualWidth")
+                {
+                    RelativeSource = new RelativeSource(RelativeSourceMode.Self)
+                });
+            }
+            else if (this.ActualHeight > 0)
+            {
+                BindingOperations.SetBinding(this, WidthProperty, new Binding("ActualHeight")
+                {
+                    RelativeSource = new RelativeSource(RelativeSourceMode.Self)
+                });
+            }
+            else
+            {
+                return;
+            }
+            this.SizeChanged -= this.OnSizeChanged;
         }
     }
 }
