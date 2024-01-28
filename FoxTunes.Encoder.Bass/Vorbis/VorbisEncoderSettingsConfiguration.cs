c89c0fd52398a2e7c2edd740405c4b2bcaa5c318
pudding <pudding@raimusoft.com>
2024-01-28 14:03:42 +0000

    Adds Ogg Vorbis encoder.

---
+++ FoxTunes.Encoder.Bass/Vorbis/VorbisEncoderSettingsConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace FoxTunes
+{
+    public static class VorbisEncoderSettingsConfiguration
+    {
+        public const string SECTION = BassEncoderBehaviourConfiguration.SECTION;
+
+        public const string QUALITY_ELEMENT = "AAAA4AE0-3259-40F8-8C64-D1FBF075DD3E";
+
+        public const int MIN_QUALITY = 1;
+
+        public const int MAX_QUALITY = 10;
+
+        public const int DEFAULT_QUALITY = 5;
+
+        public static IEnumerable<ConfigurationSection> GetConfigurationSections(IBassEncoderSettings settings)
+        {
+            yield return new ConfigurationSection(SECTION, "Converter")
+                .WithElement(new IntegerConfigurationElement(QUALITY_ELEMENT, "Quality", path: settings.Name)
+                    .WithValue(DEFAULT_QUALITY)
+                    .WithValidationRule(new IntegerValidationRule(MIN_QUALITY, MAX_QUALITY))
+            );
+        }
+    }
+}
