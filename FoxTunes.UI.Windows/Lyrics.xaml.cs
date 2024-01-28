2a368770124784afe6eee5771a4b4ab4c5c52e05
pudding <pudding@raimusoft.com>
2024-01-28 14:03:59 +0000

    Adds lyrics to right hand panel.
     This requires the lyrics meta data setting.

---
+++ FoxTunes.UI.Windows/Lyrics.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace FoxTunes
+{
+    /// <summary>
+    /// Interaction logic for Lyrics.xaml
+    /// </summary>
+    [UIComponent("F7774E81-26FC-4F0C-8E0A-67214D155547", UIComponentSlots.TOP_RIGHT, "Lyrics")]
+    [UIComponentDependency(MetaDataBehaviourConfiguration.SECTION, MetaDataBehaviourConfiguration.ENABLE_ELEMENT)]
+    public partial class Lyrics : UIComponentBase
+    {
+        public Lyrics()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
