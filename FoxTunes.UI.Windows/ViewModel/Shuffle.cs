4b9298233afcafb247d49905e281153dc3c96efa
pudding <pudding@raimusoft.com>
2024-01-28 14:04:43 +0000

    Adds toggle shuffle button to playback controls.
     I'm not 100% happy with the glyph.

---
+++ FoxTunes.UI.Windows/ViewModel/Shuffle.cs
@@ -0,0 +1,52 @@
+﻿using FoxTunes.Interfaces;
+using System;
+using System.Windows;
+
+namespace FoxTunes.ViewModel
+{
+    public class Shuffle : ViewModelBase
+    {
+        public IConfiguration Configuration { get; private set; }
+
+        private BooleanConfigurationElement _Enabled { get; set; }
+
+        public BooleanConfigurationElement Enabled
+        {
+            get
+            {
+                return this._Enabled;
+            }
+            set
+            {
+                this._Enabled = value;
+                this.OnEnabledChanged();
+            }
+        }
+
+        protected virtual void OnEnabledChanged()
+        {
+            if (this.EnabledChanged != null)
+            {
+                this.EnabledChanged(this, EventArgs.Empty);
+            }
+            this.OnPropertyChanged("Enabled");
+        }
+
+        public event EventHandler EnabledChanged;
+
+        public override void InitializeComponent(ICore core)
+        {
+            this.Configuration = this.Core.Components.Configuration;
+            this.Enabled = this.Configuration.GetElement<BooleanConfigurationElement>(
+                PlaylistBehaviourConfiguration.SECTION,
+                PlaylistBehaviourConfiguration.SHUFFLE_ELEMENT
+            );
+            base.InitializeComponent(core);
+        }
+
+        protected override Freezable CreateInstanceCore()
+        {
+            return new Shuffle();
+        }
+    }
+}
