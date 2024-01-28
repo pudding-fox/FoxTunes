afa198eb01924f5188443209ad5b9891ff67ddb7
pudding <pudding@raimusoft.com>
2024-01-28 14:04:04 +0000

    Adds CommandConfigurationElement.
     This can be used to create buttons in the settings screen.

---
+++ FoxTunes.Core/Configuration/CommandConfigurationElement.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace FoxTunes
+{
+    [Serializable]
+    public class CommandConfigurationElement : ConfigurationElement
+    {
+        public CommandConfigurationElement(string id, string name = null, string description = null, string path = null) : base(id, name, description, path)
+        {
+        }
+
+        public void Invoke()
+        {
+            if (this.Invoked != null)
+            {
+                this.Invoked(this, EventArgs.Empty);
+            }
+        }
+
+        [field: NonSerialized]
+        public EventHandler Invoked;
+
+        public CommandConfigurationElement WithHandler(Action action)
+        {
+            var handler = new EventHandler((sender, e) =>
+            {
+                try
+                {
+                    action();
+                }
+                catch (Exception exception)
+                {
+                    this.OnError(exception);
+                }
+            });
+            this.Invoked += handler;
+            return this;
+        }
+
+        public override void Reset()
+        {
+            //Nothing to do.
+        }
+    }
+}
---
+++ FoxTunes.UI.Windows/ViewModel/Config/CommandConfigurationElement.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace FoxTunes.ViewModel.Config
+{
+    public class CommandConfigurationElement : ViewModelBase
+    {
+        public static readonly DependencyProperty ElementProperty = DependencyProperty.Register(
+           "Element",
+           typeof(global::FoxTunes.CommandConfigurationElement),
+           typeof(CommandConfigurationElement),
+           new PropertyMetadata(new PropertyChangedCallback(OnElementChanged))
+       );
+
+        public static global::FoxTunes.CommandConfigurationElement GetElement(ViewModelBase source)
+        {
+            return (global::FoxTunes.CommandConfigurationElement)source.GetValue(ElementProperty);
+        }
+
+        public static void SetElement(ViewModelBase source, global::FoxTunes.CommandConfigurationElement value)
+        {
+            source.SetValue(ElementProperty, value);
+        }
+
+        public static void OnElementChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            var viewModel = sender as CommandConfigurationElement;
+            if (viewModel == null)
+            {
+                return;
+            }
+            viewModel.OnElementChanged();
+        }
+
+        public global::FoxTunes.CommandConfigurationElement Element
+        {
+            get
+            {
+                return this.GetValue(ElementProperty) as global::FoxTunes.CommandConfigurationElement;
+            }
+            set
+            {
+                this.SetValue(ElementProperty, value);
+            }
+        }
+
+        protected virtual void OnElementChanged()
+        {
+            this.OnInvokeCommandChanged();
+            if (this.ElementChanged != null)
+            {
+                this.ElementChanged(this, EventArgs.Empty);
+            }
+            this.OnPropertyChanged("Element");
+        }
+
+        public event EventHandler ElementChanged;
+
+        public ICommand InvokeCommand
+        {
+            get
+            {
+                if (this.Element == null)
+                {
+                    return null;
+                }
+                return CommandFactory.Instance.CreateCommand(
+                    this.Element.Invoke
+                );
+            }
+        }
+
+        protected virtual void OnInvokeCommandChanged()
+        {
+            if (this.InvokeCommandChanged != null)
+            {
+                this.InvokeCommandChanged(this, EventArgs.Empty);
+            }
+            this.OnPropertyChanged("InvokeCommand");
+        }
+
+        public event EventHandler InvokeCommandChanged;
+
+        protected override Freezable CreateInstanceCore()
+        {
+            return new CommandConfigurationElement();
+        }
+    }
+}
