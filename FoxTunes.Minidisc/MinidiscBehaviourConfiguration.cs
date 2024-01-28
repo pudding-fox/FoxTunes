aaab1b469736a6f0d053facfb4d34c0ce510f843
pudding <pudding@raimusoft.com>
2024-01-28 14:05:51 +0000

    WIP

--- FoxTunes.Minidisc/MinidiscBehaviourConfiguration.cs
+++ FoxTunes.Minidisc/MinidiscBehaviourConfiguration.cs
@@ -8,6 +8,10 @@ namespace FoxTunes
 
         public const string ENABLED = "AAAACAEF-31D9-49B6-A0A9-0B3E55050D0C";
 
+        public const string DISC_TITLE_SCRIPT = "BBBBF58E-1129-4926-A54B-B1BD7E522756";
+
+        public const string TRACK_NAME_SCRIPT = "CCCC3251-B5B6-4A66-AE94-68EA093BC93D";
+
         public static IEnumerable<ConfigurationSection> GetConfigurationSections()
         {
             yield return new ConfigurationSection(SECTION, Strings.MinidiscBehaviourConfiguration_Section)
