a4a3c28769d4152e8d71b1cff3b003a102b0195e
pudding <pudding@raimusoft.com>
2024-01-28 14:03:28 +0000

    Adds slider controls for Integer and Double ConfigurationElements.

---
+++ FoxTunes.UI.Windows/IntegerConfigurationElement.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace FoxTunes.Config
+{
+    /// <summary>
+    /// Interaction logic for IntegerConfigurationElement.xaml
+    /// </summary>
+    public partial class IntegerConfigurationElement : UserControl
+    {
+        public IntegerConfigurationElement()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
