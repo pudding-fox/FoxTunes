da122ca4b917bd20903d6eb3c8aa686dfb83d71b
pudding <pudding@raimusoft.com>
2024-01-28 14:07:13 +0000

    feat: adds layout editor keyboard shortcut

---
+++ FoxTunes.UI.Windows.Layout/Behaviours/LayoutDesignerKeyBindingsBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using FoxTunes.Interfaces;
+using FoxTunes.ViewModel;
+using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace FoxTunes
+{
+    [WindowsUserInterfaceDependency]
+    public class LayoutDesignerKeyBindingsBehaviour : KeyBindingsBehaviourBase
+    {
+        public ISignalEmitter SignalEmitter { get; private set; }
+
+        public IConfiguration Configuration { get; private set; }
+
+        public TextConfigurationElement Edit { get; private set; }
+
+        public ICommand EditCommand
+        {
+            get
+            {
+                return CommandFactory.Instance.CreateCommand(() =>
+                    LayoutDesignerBehaviour.Instance.IsDesigning = !LayoutDesignerBehaviour.Instance.IsDesigning
+                );
+            }
+        }
+
+        public override void InitializeComponent(ICore core)
+        {
+            this.SignalEmitter = core.Components.SignalEmitter;
+            this.Configuration = core.Components.Configuration;
+            this.Edit = this.Configuration.GetElement<TextConfigurationElement>(
+                LayoutDesignerKeyBindingsBehaviourConfiguration.SECTION,
+                LayoutDesignerKeyBindingsBehaviourConfiguration.EDIT_ELEMENT
+            );
+            if (this.Edit != null)
+            {
+                this.Commands.Add(this.Edit, this.EditCommand);
+                this.Edit.ValueChanged += this.OnValueChanged;
+            }
+            base.InitializeComponent(core);
+        }
+
+        public override IEnumerable<ConfigurationSection> GetConfigurationSections()
+        {
+            return LayoutDesignerKeyBindingsBehaviourConfiguration.GetConfigurationSections();
+        }
+    }
+}
