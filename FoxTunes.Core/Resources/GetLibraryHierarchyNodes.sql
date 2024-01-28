8f28a9926ff54ad595e4aeb1dd785dc2293fe3e3
pudding <pudding@raimusoft.com>
2024-01-28 14:03:03 +0000

    Filter is working but very slow.

--- FoxTunes.Core/Resources/GetLibraryHierarchyNodes.sql
+++ FoxTunes.Core/Resources/GetLibraryHierarchyNodes.sql
@@ -1,8 +1,34 @@
-﻿SELECT "LibraryHierarchyItems"."LibraryHierarchy_Id", "LibraryHierarchyItems"."LibraryHierarchyLevel_Id", "LibraryHierarchyItems"."DisplayValue","LibraryHierarchyItems"."IsLeaf"
+﻿WITH RECURSIVE LibraryHierarchy("Root", "Id", "Parent_Id", "LibraryHierarchy_Id", "LibraryHierarchyLevel_Id", "DisplayValue", "IsLeaf")
+AS
+(
+	SELECT "LibraryHierarchyItems"."Id", "LibraryHierarchyItems"."Id", "LibraryHierarchyItems"."Parent_Id", "LibraryHierarchyItems"."LibraryHierarchy_Id", "LibraryHierarchyItems"."LibraryHierarchyLevel_Id", "LibraryHierarchyItems"."DisplayValue","LibraryHierarchyItems"."IsLeaf"
+	FROM "LibraryHierarchyItems"
+		JOIN  "LibraryHierarchyItems" AS "LibraryHierarchyItems_Copy" ON "LibraryHierarchyItems"."Parent_Id" = "LibraryHierarchyItems_Copy"."Id"
+	WHERE "LibraryHierarchyItems"."LibraryHierarchy_Id" = @libraryHierarchyId 
+		AND "LibraryHierarchyItems_Copy"."LibraryHierarchy_Id" = @libraryHierarchyId 
+		AND "LibraryHierarchyItems_Copy"."LibraryHierarchyLevel_Id" = @libraryHierarchyLevelId 
+		AND "LibraryHierarchyItems_Copy"."DisplayValue" = @displayValue
+	UNION ALL 
+	SELECT "Root", "LibraryHierarchyItems"."Id", "LibraryHierarchyItems"."Parent_Id", "LibraryHierarchyItems"."LibraryHierarchy_Id", "LibraryHierarchyItems"."LibraryHierarchyLevel_Id", "LibraryHierarchyItems"."DisplayValue", "LibraryHierarchyItems"."IsLeaf"
+	FROM "LibraryHierarchyItems"
+		JOIN LibraryHierarchy ON "LibraryHierarchyItems"."Parent_Id" = LibraryHierarchy."Id"
+)
+
+SELECT "LibraryHierarchyItems"."LibraryHierarchy_Id", "LibraryHierarchyItems"."LibraryHierarchyLevel_Id", "LibraryHierarchyItems"."DisplayValue","LibraryHierarchyItems"."IsLeaf"
 FROM "LibraryHierarchyItems"
-JOIN  "LibraryHierarchyItems" AS "LibraryHierarchyItems_Copy" ON "LibraryHierarchyItems"."Parent_Id" = "LibraryHierarchyItems_Copy"."Id"
+	JOIN  "LibraryHierarchyItems" AS "LibraryHierarchyItems_Copy" ON "LibraryHierarchyItems"."Parent_Id" = "LibraryHierarchyItems_Copy"."Id"
 WHERE "LibraryHierarchyItems"."LibraryHierarchy_Id" = @libraryHierarchyId 
 	AND "LibraryHierarchyItems_Copy"."LibraryHierarchy_Id" = @libraryHierarchyId 
 	AND "LibraryHierarchyItems_Copy"."LibraryHierarchyLevel_Id" = @libraryHierarchyLevelId 
 	AND "LibraryHierarchyItems_Copy"."DisplayValue" = @displayValue
+	AND 
+	(
+		@filter IS NULL OR EXISTS
+		(
+			SELECT * 
+			FROM LibraryHierarchy 
+			WHERE "Root" = "LibraryHierarchyItems"."Id" 
+				AND "DisplayValue" LIKE @filter
+		)
+	)
 GROUP BY "LibraryHierarchyItems"."LibraryHierarchy_Id", "LibraryHierarchyItems"."LibraryHierarchyLevel_Id", "LibraryHierarchyItems"."DisplayValue", "LibraryHierarchyItems"."IsLeaf"
--- FoxTunes.Core/Resources/GetLibraryHierarchyRootNodes.sql
+++ FoxTunes.Core/Resources/GetLibraryHierarchyRootNodes.sql
@@ -1,4 +1,24 @@
-﻿SELECT "LibraryHierarchy_Id", "LibraryHierarchyLevel_Id", "DisplayValue", "IsLeaf"
+﻿WITH RECURSIVE LibraryHierarchy("Root", "Id", "Parent_Id", "LibraryHierarchy_Id", "LibraryHierarchyLevel_Id", "DisplayValue", "IsLeaf")
+AS
+(
+	SELECT "Id", "Id", "Parent_Id", "LibraryHierarchy_Id", "LibraryHierarchyLevel_Id", "DisplayValue", "IsLeaf"
+	FROM "LibraryHierarchyItems"
+	UNION ALL 
+	SELECT "Root", "LibraryHierarchyItems"."Id", "LibraryHierarchyItems"."Parent_Id", "LibraryHierarchyItems"."LibraryHierarchy_Id", "LibraryHierarchyItems"."LibraryHierarchyLevel_Id", "LibraryHierarchyItems"."DisplayValue", "LibraryHierarchyItems"."IsLeaf"
+	FROM "LibraryHierarchyItems"
+	JOIN LibraryHierarchy ON "LibraryHierarchyItems"."Parent_Id" = LibraryHierarchy."Id"
+)
+
+SELECT "LibraryHierarchy_Id", "LibraryHierarchyLevel_Id", "DisplayValue", "IsLeaf"
 FROM "LibraryHierarchyItems"
-WHERE "LibraryHierarchy_Id" = @libraryHierarchyId AND "Parent_Id" IS NULL
+WHERE "LibraryHierarchy_Id" = @libraryHierarchyId AND "Parent_Id" IS NULL AND 
+	(
+		@filter IS NULL OR EXISTS
+		(
+			SELECT * 
+			FROM LibraryHierarchy 
+			WHERE "Root" = FIRST("LibraryHierarchyItems"."Id")
+				AND "DisplayValue" LIKE @filter
+		)
+	)
 GROUP BY "LibraryHierarchy_Id", "LibraryHierarchyLevel_Id", "DisplayValue", "IsLeaf"
