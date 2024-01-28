a3ffcdd382427cb328f476bd36086e5bfe9094ea
pudding <pudding@raimusoft.com>
2024-01-28 14:06:43 +0000

    Begin stubbing out LayoutTree dialog.

---
+++ FoxTunes.UI.Windows.Layout/LayoutTreeWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace FoxTunes
+{
+    /// <summary>
+    /// Interaction logic for LayoutTreeWindow.xaml
+    /// </summary>
+    public partial class LayoutTreeWindow : WindowBase
+    {
+        public const string ID = "345100C4-3E83-417C-963C-8E6F88902285";
+
+        public LayoutTreeWindow()
+        {
+            if (!global::FoxTunes.Properties.Settings.Default.LayoutTreeWindowBounds.IsEmpty())
+            {
+                if (ScreenHelper.WindowBoundsVisible(global::FoxTunes.Properties.Settings.Default.LayoutTreeWindowBounds))
+                {
+                    this.Left = global::FoxTunes.Properties.Settings.Default.LayoutTreeWindowBounds.Left;
+                    this.Top = global::FoxTunes.Properties.Settings.Default.LayoutTreeWindowBounds.Top;
+                }
+                this.Width = global::FoxTunes.Properties.Settings.Default.LayoutTreeWindowBounds.Width;
+                this.Height = global::FoxTunes.Properties.Settings.Default.LayoutTreeWindowBounds.Height;
+            }
+            else
+            {
+                this.Width = 300;
+                this.Height = 600;
+            }
+            if (double.IsNaN(this.Left) || double.IsNaN(this.Top))
+            {
+                this.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            }
+            this.InitializeComponent();
+        }
+
+        public override string Id
+        {
+            get
+            {
+                return ID;
+            }
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            global::FoxTunes.Properties.Settings.Default.LayoutTreeWindowBounds = this.RestoreBounds;
+            global::FoxTunes.Properties.Settings.Default.Save();
+            base.OnClosing(e);
+        }
+    }
+}
--- FoxTunes.UI.Windows.Layout/Properties/Settings.Designer.cs
+++ FoxTunes.UI.Windows.Layout/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace FoxTunes.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.6.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.3.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -32,6 +32,18 @@ namespace FoxTunes.Properties {
             }
             set {
                 this["ToolWindowBounds"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0,0,0,0")]
+        public global::System.Windows.Rect LayoutTreeWindowBounds {
+            get {
+                return ((global::System.Windows.Rect)(this["LayoutTreeWindowBounds"]));
+            }
+            set {
+                this["LayoutTreeWindowBounds"] = value;
             }
         }
     }
