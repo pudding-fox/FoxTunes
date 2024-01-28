8309670be1af7e97978a77b5998033122bfb4f0e
pudding <pudding@raimusoft.com>
2024-01-28 14:06:31 +0000

    Adds RMS toggle to wave form context menu

--- FoxTunes.UI.Windows.WaveBar/Behaviours/WaveBarBehaviour.cs
+++ FoxTunes.UI.Windows.WaveBar/Behaviours/WaveBarBehaviour.cs
@@ -14,12 +14,18 @@ namespace FoxTunes
 
         public SelectionConfigurationElement Mode { get; private set; }
 
+        public BooleanConfigurationElement Rms { get; private set; }
+
         public override void InitializeComponent(ICore core)
         {
             this.Configuration = core.Components.Configuration;
             this.Mode = this.Configuration.GetElement<SelectionConfigurationElement>(
                 WaveBarBehaviourConfiguration.SECTION,
                 WaveBarBehaviourConfiguration.MODE_ELEMENT
+            );
+            this.Rms = this.Configuration.GetElement<BooleanConfigurationElement>(
+                WaveBarBehaviourConfiguration.SECTION,
+                WaveBarBehaviourConfiguration.RMS_ELEMENT
             );
             base.InitializeComponent(core);
         }
@@ -42,14 +48,27 @@ namespace FoxTunes
                         CATEGORY,
                         option.Id,
                         option.Name,
+                        path: this.Mode.Name,
                         attributes: this.Mode.Value == option ? InvocationComponent.ATTRIBUTE_SELECTED : InvocationComponent.ATTRIBUTE_NONE);
                 }
+                yield return new InvocationComponent(
+                    CATEGORY,
+                    this.Rms.Id,
+                    this.Rms.Name,
+                    attributes: this.Rms.Value ? InvocationComponent.ATTRIBUTE_SELECTED : InvocationComponent.ATTRIBUTE_NONE);
             }
         }
 
         public Task InvokeAsync(IInvocationComponent component)
         {
-            this.Mode.Value = this.Mode.Options.FirstOrDefault(option => string.Equals(option.Id, component.Id));
+            if (string.Equals(this.Mode.Name, component.Path))
+            {
+                this.Mode.Value = this.Mode.Options.FirstOrDefault(option => string.Equals(option.Id, component.Id));
+            }
+            else if (string.Equals(this.Rms.Name, component.Name))
+            {
+                this.Rms.Toggle();
+            }
             this.Configuration.Save();
 #if NET40
             return TaskEx.FromResult(false);
