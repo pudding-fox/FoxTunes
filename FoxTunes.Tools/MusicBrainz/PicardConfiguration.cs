87dab9ea1601914b759f8a97d63a900a6db258b5
pudding <pudding@raimusoft.com>
2024-01-28 14:03:50 +0000

    Enable Picard by default if it's installed.

--- FoxTunes.Tools/MusicBrainz/PicardConfiguration.cs
+++ FoxTunes.Tools/MusicBrainz/PicardConfiguration.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 
 namespace FoxTunes
 {
     public static class PicardConfiguration
     {
+        const string PATH = "C:\\Program Files\\MusicBrainz Picard\\picard.exe";
+
         public const string SECTION = ToolsConfiguration.SECTION;
 
         public const string ENABLED_ELEMENT = "AAAA9D6F-71B7-4C80-AA30-F952576F378A";
@@ -13,8 +16,8 @@ namespace FoxTunes
         public static IEnumerable<ConfigurationSection> GetConfigurationSections()
         {
             yield return new ConfigurationSection(SECTION, "Tools")
-                .WithElement(new BooleanConfigurationElement(ENABLED_ELEMENT, "Enabled", path: "MusicBrainz Picard").WithValue(false))
-                .WithElement(new TextConfigurationElement(PATH_ELEMENT, "Path", path: "MusicBrainz Picard").WithValue("C:\\Program Files\\MusicBrainz Picard\\picard.exe").WithFlags(ConfigurationElementFlags.FileName)
+                .WithElement(new BooleanConfigurationElement(ENABLED_ELEMENT, "Enabled", path: "MusicBrainz Picard").WithValue(File.Exists(PATH)))
+                .WithElement(new TextConfigurationElement(PATH_ELEMENT, "Path", path: "MusicBrainz Picard").WithValue(PATH).WithFlags(ConfigurationElementFlags.FileName)
             );
             StandardComponents.Instance.Configuration.GetElement<BooleanConfigurationElement>(SECTION, ENABLED_ELEMENT).ConnectValue(value => UpdateConfiguration(value));
         }
