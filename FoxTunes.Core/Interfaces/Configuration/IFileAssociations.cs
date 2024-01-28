548fe428992220484da73e7a2a5ff2bd31dc3cea
pudding <pudding@raimusoft.com>
2024-01-28 14:03:14 +0000

    Adds file association framework.

---
+++ FoxTunes.Core/Interfaces/Configuration/IFileAssociations.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace FoxTunes.Interfaces
+{
+    public interface IFileAssociations
+    {
+        IEnumerable<IFileAssociation> Associations { get; }
+
+        IFileAssociation Create(string extension);
+
+        void Enable();
+
+        void Enable(IEnumerable<IFileAssociation> associations);
+
+        void Disable();
+
+        void Disable(IEnumerable<IFileAssociation> associations);
+    }
+}
