eb0bed714a7bdccbf1a4b2e11c3234670ae72f03
pudding <pudding@raimusoft.com>
2024-01-28 14:03:21 +0000

    Adds custom window chrome.
     Adds rounded corners to mini player.

---
+++ FoxTunes.UI.Windows/Behaviours/WindowStateBehaviour.cs
@@ -0,0 +1,178 @@
+﻿using FoxTunes.Interfaces;
+using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace FoxTunes
+{
+    public class WindowStateBehaviour : StandardBehaviour, IDisposable
+    {
+        const int WM_GETMINMAXINFO = 0x0024;
+
+        public WindowStateBehaviour()
+        {
+            this.Hook = new HwndSourceHook(this.WindowProc);
+        }
+
+        public HwndSourceHook Hook { get; private set; }
+
+        public void Enable()
+        {
+            if (Application.Current != null && Application.Current.MainWindow != null)
+            {
+                var handle = new WindowInteropHelper(Application.Current.MainWindow).Handle;
+                HwndSource.FromHwnd(handle).AddHook(this.Hook);
+            }
+        }
+
+        public void Disable()
+        {
+            if (Application.Current != null && Application.Current.MainWindow != null)
+            {
+                var handle = new WindowInteropHelper(Application.Current.MainWindow).Handle;
+                HwndSource.FromHwnd(handle).RemoveHook(this.Hook);
+            }
+        }
+
+        protected virtual IntPtr WindowProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            switch (msg)
+            {
+                case WM_GETMINMAXINFO:
+                    this.WmGetMinMaxInfo(hwnd, lParam);
+                    break;
+            }
+            return IntPtr.Zero;
+        }
+
+        protected virtual void WmGetMinMaxInfo(IntPtr hwnd, IntPtr lParam)
+        {
+            var mousePosition = default(POINT);
+            GetCursorPos(out mousePosition);
+
+            var primaryScreen = MonitorFromPoint(new POINT(0, 0), MonitorOptions.MONITOR_DEFAULTTOPRIMARY);
+            var primaryScreenInfo = new MONITORINFO();
+            if (GetMonitorInfo(primaryScreen, primaryScreenInfo) == false)
+            {
+                return;
+            }
+
+            var currentScreen = MonitorFromPoint(mousePosition, MonitorOptions.MONITOR_DEFAULTTONEAREST);
+            var minMaxInfo = (MINMAXINFO)Marshal.PtrToStructure(lParam, typeof(MINMAXINFO));
+
+            if (primaryScreen == currentScreen)
+            {
+                minMaxInfo.MaxPosition.X = primaryScreenInfo.Work.Left;
+                minMaxInfo.MaxPosition.Y = primaryScreenInfo.Work.Top;
+                minMaxInfo.MaxSize.X = primaryScreenInfo.Work.Right - primaryScreenInfo.Work.Left;
+                minMaxInfo.MaxSize.Y = primaryScreenInfo.Work.Bottom - primaryScreenInfo.Work.Top;
+            }
+            else
+            {
+                minMaxInfo.MaxPosition.X = primaryScreenInfo.Monitor.Left;
+                minMaxInfo.MaxPosition.Y = primaryScreenInfo.Monitor.Top;
+                minMaxInfo.MaxSize.X = primaryScreenInfo.Monitor.Right - primaryScreenInfo.Monitor.Left;
+                minMaxInfo.MaxSize.Y = primaryScreenInfo.Monitor.Bottom - primaryScreenInfo.Monitor.Top;
+            }
+
+            Marshal.StructureToPtr(minMaxInfo, lParam, true);
+        }
+
+        public override void InitializeComponent(ICore core)
+        {
+            if (Application.Current != null && Application.Current.MainWindow != null)
+            {
+                Application.Current.MainWindow.Loaded += (sender, e) => this.Enable();
+            }
+            base.InitializeComponent(core);
+        }
+
+        public bool IsDisposed { get; private set; }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (this.IsDisposed || !disposing)
+            {
+                return;
+            }
+            this.OnDisposing();
+            this.IsDisposed = true;
+        }
+
+        protected virtual void OnDisposing()
+        {
+            this.Disable();
+        }
+
+        ~WindowStateBehaviour()
+        {
+            Logger.Write(this, LogLevel.Error, "Component was not disposed: {0}", this.GetType().Name);
+            this.Dispose(true);
+        }
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetCursorPos(out POINT lpPoint);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr MonitorFromPoint(POINT pt, MonitorOptions dwFlags);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetMonitorInfo(IntPtr hMonitor, MONITORINFO lpmi);
+
+        public enum MonitorOptions : uint
+        {
+            MONITOR_DEFAULTTONULL = 0x00000000,
+            MONITOR_DEFAULTTOPRIMARY = 0x00000001,
+            MONITOR_DEFAULTTONEAREST = 0x00000002
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+
+            public POINT(int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MINMAXINFO
+        {
+            public POINT Reserved;
+            public POINT MaxSize;
+            public POINT MaxPosition;
+            public POINT MinTrackSize;
+            public POINT MaxTrackSize;
+        };
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public class MONITORINFO
+        {
+            public int cbSize = Marshal.SizeOf(typeof(MONITORINFO));
+            public RECT Monitor = new RECT();
+            public RECT Work = new RECT();
+            public int Flags = 0;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+    }
+}
---
+++ FoxTunes.UI.Windows/Titlebar.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace FoxTunes
+{
+    /// <summary>
+    /// Interaction logic for Titlebar.xaml
+    /// </summary>
+    public partial class Titlebar : UserControl
+    {
+        public Titlebar()
+        {
+            this.InitializeComponent();
+        }
+
+        protected virtual void OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                if (Application.Current != null && Application.Current.MainWindow != null)
+                {
+                    Application.Current.MainWindow.DragMove();
+                }
+            }
+        }
+    }
+}
