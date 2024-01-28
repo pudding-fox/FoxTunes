db841d25d052c41333a223ee4b8e0c160eb399d1
pudding <pudding@raimusoft.com>
2024-01-28 14:03:54 +0000

    Adds library roots to settings dialog.
     It's not very useful yet.

---
+++ FoxTunes.UI.Windows/LibraryRootsDialog.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace FoxTunes
+{
+    /// <summary>
+    /// Interaction logic for LibraryRootsDialog.xaml
+    /// </summary>
+    public partial class LibraryRootsDialog : UserControl
+    {
+        public LibraryRootsDialog()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
