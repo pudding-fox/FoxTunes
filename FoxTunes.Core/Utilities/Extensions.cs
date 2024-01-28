0612e3faff5c2e7653bb3f3bbda966e522ec8cb7
pudding <pudding@raimusoft.com>
2024-01-28 14:05:43 +0000

    Fix library items without meta data cannot be added to playlist.
     Also fix AsyncDebouncer error when setting result of completed task.

---
+++ FoxTunes.Core/Utilities/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace FoxTunes
+{
+    public static partial class Extensions
+    {
+        public static Exception Unwrap(this AggregateException exception)
+        {
+            var exceptions = exception.InnerExceptions.ToArray();
+            switch (exceptions.Length)
+            {
+                case 0:
+                    //This shouldn't happen.
+                    return exception;
+                case 1:
+                    return exceptions[0];
+                default:
+                    return exception;
+            }
+        }
+    }
+}
