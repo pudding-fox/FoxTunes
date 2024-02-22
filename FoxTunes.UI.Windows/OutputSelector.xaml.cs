62dce4cd5bf1a1faa70b19ae307b4db4cb8a4124
sophie <sophie@raimusoft.com>
2024-02-22 14:40:30 +0000

    chore: adds output selector and tempo to the toolbar
    disabled by default

--- FoxTunes.UI.Windows/OutputSelector.xaml.cs
+++ FoxTunes.UI.Windows/OutputSelector.xaml.cs
@@ -4,6 +4,7 @@
     /// Interaction logic for OutputSelector.xaml
     /// </summary>
     [UIComponent("7B28BE96-30AB-4911-B284-36C16CE6A168", role: UIComponentRole.System)]
+    [UIComponentToolbar(1010, UIComponentToolbarAlignment.Right, false)]
     public partial class OutputSelector : UIComponentBase
     {
         public OutputSelector()
--- FoxTunes.UI.Windows/Tempo.xaml.cs
+++ FoxTunes.UI.Windows/Tempo.xaml.cs
@@ -4,6 +4,7 @@
     /// Interaction logic for Tempo.xaml
     /// </summary>
     [UIComponent("F7526CA2-23D4-4842-80F1-4EBC648187CC", role: UIComponentRole.Launcher)]
+    [UIComponentToolbar(1110, UIComponentToolbarAlignment.Right, false)]
     public partial class Tempo : UIComponentBase
     {
         public Tempo()
