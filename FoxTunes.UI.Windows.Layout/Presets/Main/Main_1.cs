767b625317819659995c745c2eb2d4f1fb9ff6c1
pudding <sophiesticated.fox@gmail.com>
2024-08-08 17:39:06 +0100

    fix: set default layout preset

--- FoxTunes.UI.Windows.Layout/Presets/Main/Main_1.cs
+++ FoxTunes.UI.Windows.Layout/Presets/Main/Main_1.cs
@@ -5,7 +5,7 @@
     {
         public const string ID = "AAAA5819-A188-4B16-A0D9-825FC4150E5B";
 
-        public Main_1() : base(ID, CATEGORY_MAIN, Strings.UIComponentLayoutProviderPresets_Main_1, Resources.Main_1)
+        public Main_1() : base(ID, CATEGORY_MAIN, Strings.UIComponentLayoutProviderPresets_Main_1, Resources.Main_1, true)
         {
 
         }
--- FoxTunes.UI.Windows.Layout/UIComponentLayoutProviderPreset.cs
+++ FoxTunes.UI.Windows.Layout/UIComponentLayoutProviderPreset.cs
@@ -8,12 +8,13 @@ namespace FoxTunes
     {
         public const string CATEGORY_MAIN = "2CE18904-A499-4CBF-8BFA-E4D594F7D37F";
 
-        protected UIComponentLayoutProviderPreset(string id, string category, string name, string layout)
+        protected UIComponentLayoutProviderPreset(string id, string category, string name, string layout, bool @default = false)
         {
             this.Id = id;
             this.Category = category;
             this.Name = name;
             this.Layout = layout;
+            this.Default = @default;
         }
 
         public string Id { get; private set; }
@@ -24,13 +25,20 @@ namespace FoxTunes
 
         public string Layout { get; private set; }
 
+        public bool Default { get; private set; }
+
         public IEnumerable<ConfigurationSection> GetConfigurationSections()
         {
+            var option = new SelectionConfigurationOption(this.Id, this.Name);
+            if (this.Default)
+            {
+                option = option.Default();
+            }
             yield return new ConfigurationSection(GetSection(this.Category))
                 .WithElement(new SelectionConfigurationElement(GetPreset(this.Category))
                 .WithOptions(new[]
                 {
-                    new SelectionConfigurationOption(this.Id, this.Name)
+                   option
                 })
             );
         }
