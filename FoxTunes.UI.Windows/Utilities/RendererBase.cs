c1daa812028e5e6720c1d7bb1bf24b557f9e900a
sophie <sophie@raimusoft.com>
2025-01-21 18:01:39 +0000

    fix: fix scrollback on grouped playlists

--- FoxTunes.UI.Windows/Utilities/RendererBase.cs
+++ FoxTunes.UI.Windows/Utilities/RendererBase.cs
@@ -1082,6 +1082,97 @@ namespace FoxTunes
                 Convert.ToByte((color1.B * ratio1) + (color2.B * ratio2))
             );
         }
+
+        public static Color WithHue(this Color color, float hue)
+        {
+            var temp = global::System.Drawing.Color.FromArgb(color.A, color.R, color.G, color.B);
+            return FromAhsb(temp.A, hue, temp.GetSaturation(), temp.GetBrightness());
+        }
+
+        public static Color FromAhsb(byte alpha, float hue, float saturation, float brightness)
+        {
+
+            if (0 > alpha || 255 < alpha)
+            {
+                throw new ArgumentOutOfRangeException("alpha", alpha,
+                  "Value must be within a range of 0 - 255.");
+            }
+            if (0f > hue || 360f < hue)
+            {
+                throw new ArgumentOutOfRangeException("hue", hue,
+                  "Value must be within a range of 0 - 360.");
+            }
+            if (0f > saturation || 1f < saturation)
+            {
+                throw new ArgumentOutOfRangeException("saturation", saturation,
+                  "Value must be within a range of 0 - 1.");
+            }
+            if (0f > brightness || 1f < brightness)
+            {
+                throw new ArgumentOutOfRangeException("brightness", brightness,
+                  "Value must be within a range of 0 - 1.");
+            }
+
+            if (0 == saturation)
+            {
+                return Color.FromArgb(
+                    alpha,
+                    Convert.ToByte(brightness * 255),
+                    Convert.ToByte(brightness * 255),
+                    Convert.ToByte(brightness * 255)
+                );
+            }
+
+            float fMax, fMid, fMin;
+            byte iSextant, iMax, iMid, iMin;
+
+            if (0.5 < brightness)
+            {
+                fMax = brightness - (brightness * saturation) + saturation;
+                fMin = brightness + (brightness * saturation) - saturation;
+            }
+            else
+            {
+                fMax = brightness + (brightness * saturation);
+                fMin = brightness - (brightness * saturation);
+            }
+
+            iSextant = (byte)Math.Floor(hue / 60f);
+            if (300f <= hue)
+            {
+                hue -= 360f;
+            }
+            hue /= 60f;
+            hue -= 2f * (float)Math.Floor(((iSextant + 1f) % 6f) / 2f);
+            if (0 == iSextant % 2)
+            {
+                fMid = hue * (fMax - fMin) + fMin;
+            }
+            else
+            {
+                fMid = fMin - hue * (fMax - fMin);
+            }
+
+            iMax = Convert.ToByte(fMax * 255);
+            iMid = Convert.ToByte(fMid * 255);
+            iMin = Convert.ToByte(fMin * 255);
+
+            switch (iSextant)
+            {
+                case 1:
+                    return Color.FromArgb(alpha, iMid, iMax, iMin);
+                case 2:
+                    return Color.FromArgb(alpha, iMin, iMax, iMid);
+                case 3:
+                    return Color.FromArgb(alpha, iMin, iMid, iMax);
+                case 4:
+                    return Color.FromArgb(alpha, iMid, iMin, iMax);
+                case 5:
+                    return Color.FromArgb(alpha, iMax, iMin, iMid);
+                default:
+                    return Color.FromArgb(alpha, iMax, iMid, iMin);
+            }
+        }
     }
 
     public class ColorStop
